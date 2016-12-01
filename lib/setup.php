11f77c08f700271ac9ffecb02ebaa0e9f298d1d5
Mattia <guzzi.mattia@gmail.com>
2016-12-01 16:14:06 +0100

    add footer menu

--- lib/setup.php
+++ lib/setup.php
@@ -30,6 +30,11 @@ function setup() {
     'primary_navigation' => __('Primary Navigation', 'sage')
   ]);
 
+
+  register_nav_menus([
+      'footer_navigation' => __('footer', 'sage')
+  ]);
+
   // Enable post thumbnails
   // http://codex.wordpress.org/Post_Thumbnails
   // http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
