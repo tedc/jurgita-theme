35e2df6e4212ab94dbb3c4718a4a0d050e4acb5d
Mattia <guzzi.mattia@gmail.com>
2016-12-01 15:35:48 +0100

    a commit

--- front-page.php
+++ front-page.php
@@ -1,4 +1,6 @@
 <?php if (has_post_thumbnail()) { ?>
     <div class="header" style="background-image: url('<?php header_image(); ?>')">
-        <span class="header-text"><?php get_bloginfo() ?></span></div>
+        <h1 class="header-title"><?php bloginfo('name'); ?> </h1>
+        <h2 class="header-subtitle"><?php bloginfo('name'); ?></h2>
+    </div>
 <?php }
