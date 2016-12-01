52c26dd97d3869087744b07f4a99f128df7267d7
Mattia <guzzi.mattia@gmail.com>
2016-12-01 17:00:45 +0100

    a commit

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,12 @@
 <footer class="footer">
-  <div class="container">
-    <h1><?php bloginfo("name"); ?></h1>
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-    <section class="widget widget_img">
-      <?php _e('Credits', 'jurgita'); ?><a class="icon-credits" target="_blank" rel="nofollow" href="http://www.bspkn.it/"></a>
-    </section>
-  </div>
+    <div class="container">
+        <h1><?php bloginfo("name"); ?></h1>
+        <div class="bottom-footer">
+            <?php dynamic_sidebar('sidebar-footer'); ?>
+            <section class="widget widget_img">
+                <?php _e('Credits', 'jurgita'); ?><a class="icon-credits" target="_blank" rel="nofollow"
+                                                     href="http://www.bspkn.it/"></a>
+            </section>
+        </div>
+    </div>
 </footer>
