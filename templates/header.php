47083570007d5ea2e84fbd530c65583372676d00
Mattia <guzzi.mattia@gmail.com>
2016-12-01 14:37:43 +0100

    commit

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,11 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+  <div class="container container-header">
+    <a class="icon icon-pittogramma" href="<?= esc_url(home_url('/')); ?>">j</a>
+
+    <div class="container-menu">
+    
+       <a class="btn-header btn-shop">shop</a>
+       <a class="btn-header btn-menu">menu</a>
+    </div>
   </div>
 </header>
