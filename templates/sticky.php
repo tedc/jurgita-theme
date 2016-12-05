84bdaafab5e436e9d1fc241ee54d61d4a94a01fa
Mattia <guzzi.mattia@gmail.com>
2016-12-05 11:16:33 +0100

    no message

--- templates/sticky.php
+++ templates/sticky.php
@@ -1,3 +1,8 @@
+<div class="container-img">
+    <img src="<?php the_post_thumbnail_url() ?>">
+    <div class="square"></div>
+    <div class="square-shadow"></div>
+</div>
 <article <?php post_class(); ?>>
     <header>
         <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
