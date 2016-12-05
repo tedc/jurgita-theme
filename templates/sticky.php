fd98607f97adbac1a3b5a23eef1ecb97e47a5e85
Mattia <guzzi.mattia@gmail.com>
2016-12-05 13:17:35 +0100

    no message

--- templates/sticky.php
+++ templates/sticky.php
@@ -7,12 +7,13 @@
 </div>
 <article <?php post_class(); ?>>
     <div class="content-post">
-    <header>
-        <?php get_template_part('templates/entry-meta'); ?>
-        <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    </header>
-    <div class="entry-summary">
-        <?php the_excerpt(); ?>
-
+        <header>
+            <?php get_template_part('templates/entry-meta'); ?>
+            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        </header>
+        <div class="entry-summary">
+            <?php the_excerpt(); ?>
+        </div>
+        <a class="btn" href="<?php the_permalink(); ?>"></a>
     </div>
 </article>
