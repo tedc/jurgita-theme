23a1a0519755d19452f7a89809f2c2c3539bd341
Mattia <guzzi.mattia@gmail.com>
2016-12-05 18:31:49 +0100

    no message

--- templates/content.php
+++ templates/content.php
@@ -1,16 +1,21 @@
 <article class="col-2" <?php post_class(); ?>>
-  <div class="container-img">
-    <figure>
-      <?php the_post_thumbnail( 'post-size' ) ?>
-    </figure>
-  </div>
+    <div class="container-img">
+        <a href="<?php the_permalink(); ?>">
+            <figure>
+                <?php the_post_thumbnail('post-size') ?>
+            </figure>
+        </a>
+        <div class="blur">
+            <i class="icon-lens">X</i>
+        </div>
+    </div>
 
-  <header>
-    <?php get_template_part('templates/entry-meta'); ?>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-  </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
-  </div>
-  <a class="btn" href="<?php the_permalink(); ?>"><?php _e('Leggi tutto', 'jurgita') ?></a>
+    <header>
+        <?php get_template_part('templates/entry-meta'); ?>
+        <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    </header>
+    <div class="entry-summary">
+        <?php the_excerpt(); ?>
+    </div>
+    <a class="btn" href="<?php the_permalink(); ?>"><?php _e('Leggi tutto', 'jurgita') ?></a>
 </article>
