e2afc87b2ac0cc2dd3daba543cdf048e423848f0
Mattia <guzzi.mattia@gmail.com>
2016-12-07 18:36:09 +0100

    no message

--- index.php
+++ index.php
@@ -49,21 +49,16 @@
         'mid_size'  => 1,
     ) ); */?>
 
-    <?php echo paginate_links( array(
-        'base'               => '%_%',
-        'format'             => '?paged=%#%',
-        'total'              => 1,
-        'current'            => 0,
-        'show_all'           => false,
-        'end_size'           => 1,
-        'mid_size'           => 2,
-        'prev_next'          => true,
-        'prev_text'          => __('« Previous'),
-        'next_text'          => __('Next »'),
-        'type'               => 'plain',
-        'add_args'           => false,
-        'add_fragment'       => '',
-        'before_page_number' => '',
-        'after_page_number'  => ''
-     )); ?>
+    <?php
+    global $wp_query;
+
+    $big = 999999999; // need an unlikely integer
+
+    echo paginate_links( array(
+        'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+        'format' => '?paged=%#%',
+        'current' => max( 1, get_query_var('paged') ),
+        'total' => $wp_query->max_num_pages
+    ) );
+    ?>
 </div>
