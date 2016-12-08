2cfa933786cfabf740566142c82b75b4f6c4907d
Mattia <guzzi.mattia@gmail.com>
2016-12-08 14:33:13 +0100

    no message

--- index.php
+++ index.php
@@ -41,21 +41,21 @@
         <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
     <?php endwhile; ?>
     <?php endif; ?>
+</div>
 
+<div class="navigation-page">
+<?php
+global $wp_query;
 
+$big = 999999999; // need an unlikely integer
 
-    <?php
-    global $wp_query;
-
-    $big = 999999999; // need an unlikely integer
-
-    echo paginate_links(array(
-        'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
-        'format' => '?paged=%#%',
-        'current' => max(1, get_query_var('paged')),
-        'total' => $wp_query->max_num_pages,
-        'prev_text' => __('Precedente', 'jurgita'),
-        'next_text' => __('Successiva', 'jurgita'),
-    ));
-    ?>
+echo paginate_links(array(
+    'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+    'format' => '?paged=%#%',
+    'current' => max(1, get_query_var('paged')),
+    'total' => $wp_query->max_num_pages,
+    'prev_text' => __('Precedente', 'jurgita'),
+    'next_text' => __('Successiva', 'jurgita'),
+));
+?>
 </div>
