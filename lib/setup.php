1391b5b552d177207e5b9f9d9b0b2009c13fe927
Mattia <guzzi.mattia@gmail.com>
2016-12-09 10:08:10 +0100

    no message

--- lib/setup.php
+++ lib/setup.php
@@ -48,6 +48,8 @@ function setup() {
   // Enable HTML5 markup support
   // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
   add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
+  
+  add_theme_support('woocommerce');
 
   // Use main stylesheet for visual editor
   // To add custom styles edit /assets/styles/layouts/_tinymce.scss
