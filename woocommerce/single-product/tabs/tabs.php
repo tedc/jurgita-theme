3ca6fc57d96047d59ba1f08fbca4b27cb9ae2885
Mattia <guzzi.mattia@gmail.com>
2016-12-09 15:16:08 +0100

    no message

--- woocommerce/single-product/tabs/tabs.php
+++ woocommerce/single-product/tabs/tabs.php
@@ -26,23 +26,23 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Each tab is an array containing title, callback and priority.
  * @see woocommerce_default_product_tabs()
  */
-$tabs = apply_filters( 'woocommerce_product_tabs', array() );
+/*$tabs = apply_filters( 'woocommerce_product_tabs', array() );*/
 
-if ( ! empty( $tabs ) ) : ?>
+/*if ( ! empty( $tabs ) ) : */?><!--
 
 	<div class="woocommerce-tabs wc-tabs-wrapper">
 		<ul class="tabs wc-tabs">
-			<?php foreach ( $tabs as $key => $tab ) : ?>
-				<li class="<?php echo esc_attr( $key ); ?>_tab">
-					<a href="#tab-<?php echo esc_attr( $key ); ?>"><?php echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', esc_html( $tab['title'] ), $key ); ?></a>
+			<?php /*foreach ( $tabs as $key => $tab ) : */?>
+				<li class="<?php /*echo esc_attr( $key ); */?>_tab">
+					<a href="#tab-<?php /*echo esc_attr( $key ); */?>"><?php /*echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', esc_html( $tab['title'] ), $key ); */?></a>
 				</li>
-			<?php endforeach; ?>
+			<?php /*endforeach; */?>
 		</ul>
-		<?php foreach ( $tabs as $key => $tab ) : ?>
-			<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr( $key ); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>">
-				<?php call_user_func( $tab['callback'], $key, $tab ); ?>
+		<?php /*foreach ( $tabs as $key => $tab ) : */?>
+			<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php /*echo esc_attr( $key ); */?> panel entry-content wc-tab" id="tab-<?php /*echo esc_attr( $key ); */?>">
+				<?php /*call_user_func( $tab['callback'], $key, $tab ); */?>
 			</div>
-		<?php endforeach; ?>
+		<?php /*endforeach; */?>
 	</div>
 
-<?php endif; ?>
+--><?php /*endif; */?>
