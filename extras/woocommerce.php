082875e7a8547ba754fecd094d98682dc2b158fb
tedc <egrandinetti@gmail.com>
2016-12-10 11:08:07 +0100

    remove comments

--- extras/woocommerce.php
+++ extras/woocommerce.php
@@ -1,8 +1,3 @@
 <?php
-<<<<<<< HEAD
-	add_filter('woocommerce_show_page_title', '__return_false');
-	remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
-=======
 add_filter('woocommerce_show_page_title', '__return_false');
->>>>>>> origin/master
 ?>
--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,6 +1,5 @@
 <div class="container-post-head">
     <time class="updated" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
     <?php the_category(",") ?>
-    <!-- aopsad-->
 </div>
 
