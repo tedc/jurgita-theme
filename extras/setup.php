49a6ba34e65cc80f52ba99f0a3713c2fdacbf33c
tedc <egrandinetti@gmail.com>
2016-12-11 19:04:59 +0100

    var dump

--- extras/setup.php
+++ extras/setup.php
@@ -1,6 +1,7 @@
 <?php
 	
 	function main_menu_footer($nav_menu, $args) {
+		var_dump($args);
 		if($args['theme_location'] === 'primary_navigation') : 
 			ob_start();
 			get_template_part( 'templates/menu', 'footer' );
