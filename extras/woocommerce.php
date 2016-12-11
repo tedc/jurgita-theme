88aef5d268f406acea4eaf2115953104d905faa7
Mattia <guzzi.mattia@gmail.com>
2016-12-11 17:17:12 +0100

    no message

--- extras/woocommerce.php
+++ extras/woocommerce.php
@@ -1,3 +1,10 @@
 <?php
 add_filter('woocommerce_show_page_title', '__return_false');
+
+add_filter( 'woocommerce_product_tabs', 'wcs_woo_remove_reviews_tab', 98 );
+function wcs_woo_remove_reviews_tab($tabs) {
+    unset($tabs['reviews']);
+    return $tabs;
+}
+
 ?>
