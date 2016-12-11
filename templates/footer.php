bec5e909c5f08afd618a44b0498d2281b77c2e99
Mattia <guzzi.mattia@gmail.com>
2016-12-11 18:55:29 +0100

    no message

--- templates/footer.php
+++ templates/footer.php
@@ -3,8 +3,9 @@
         <h2><?php bloginfo("name"); ?></h2>
         <div class="bottom-footer">
             <?php dynamic_sidebar('sidebar-footer'); ?>
-            <section class="widget">
-                <?php wp_nav_menu( array( 'menu' => 'menu footer' ) );?>
+            <section class="menu-footer">
+                <?php wp_nav_menu( array( 'menu' => 'menu footer',
+                    'menu_class' => 'menu-footer') );?>
             </section>
             <section class="widget widget_img">
                 <?php _e('Credits', 'jurgita'); ?><a class="icon-credits" target="_blank" rel="nofollow"
