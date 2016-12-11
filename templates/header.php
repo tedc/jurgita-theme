8617e1569c2dfa256d048338586a39e4df4a5a86
tedc <egrandinetti@gmail.com>
2016-12-11 11:47:45 +0100

    menu

--- templates/header.php
+++ templates/header.php
@@ -6,10 +6,10 @@
 	</a>
 	<div class="menu-container">
 		<?php icl_selector(); ?>
-		<a class="btn-header btn-shop" href="<?php echo get_permalink( $shop ); ?>" title="<?php echo get_the_title($shop); ?>"><?php _e('Shop', 'jurgita'); ?></a>
+		<a class="btn-header btn-shop" href="<?php echo get_permalink( $shop ); ?>" title="<?php echo get_the_title($shop); ?>"><span class="btn-header-text"><?php _e('Shop', 'jurgita'); ?></span></a>
 		<?php wp_nav_menu( ['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container_id' => 'menu', 'container_class' => 'menu'] ); ?>
 		<a class="btn-header btn-menu" href="#" data-reveal="#menu">
-			<?php _e('Menu', 'jurgita'); ?>
+			<span class="btn-header-text"><?php _e('Menu', 'jurgita'); ?></span>
 			<span class="toggle" data-close="<?php _e('Chiudi', 'jurgita'); ?>">
 				<span class="toggle-line"></span>
 				<span class="toggle-line"></span>
