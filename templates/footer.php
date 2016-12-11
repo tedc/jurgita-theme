ef35532b454171a3868d66fe43a5e5c7e595ee6b
Mattia <guzzi.mattia@gmail.com>
2016-12-11 18:50:53 +0100

    no message

--- templates/footer.php
+++ templates/footer.php
@@ -3,6 +3,9 @@
         <h2><?php bloginfo("name"); ?></h2>
         <div class="bottom-footer">
             <?php dynamic_sidebar('sidebar-footer'); ?>
+            <section class="widget">
+                <?php wp_nav_menu( array( 'menu' => 'menu footer' ) );?>
+            </section>
             <section class="widget widget_img">
                 <?php _e('Credits', 'jurgita'); ?><a class="icon-credits" target="_blank" rel="nofollow"
                                                      href="http://www.bspkn.it/"></a>
