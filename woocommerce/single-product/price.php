9dd4c17cf24a587de4d3bec220a0de1ba9e5235b
Mattia <guzzi.mattia@gmail.com>
2016-12-12 12:32:48 +0100

    no message

--- woocommerce/single-product/price.php
+++ woocommerce/single-product/price.php
@@ -24,11 +24,15 @@ global $product;
 
 ?>
 <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-	<p class="price"><?php $price = $product->get_price_html();
-		$price_split = explode(",", $price);
-		echo $price_split[0].", divisio";
-		echo $price_split[1].",";
-		?></p>
+	<?php $price = $product->get_price_html();
+	$price_split = explode(",", $price);
+	$int_price = $price_split[0].",";
+	$cent_price = $price_split[1];
+	?>
+	<div class="price">
+		<span class="int-price"><?php echo $int_price ?></span>
+		<span class="cent-price"><?php echo $cent_price ?></span>
+	</div>
 
 	<meta itemprop="price" content="<?php echo esc_attr( $product->get_display_price() ); ?>" />
 	<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
