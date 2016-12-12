661caf806fa3371b7e77095e17a8341324367f9f
Mattia <guzzi.mattia@gmail.com>
2016-12-12 15:33:20 +0100

    no message

--- extras/woocommerce.php
+++ extras/woocommerce.php
@@ -11,7 +11,7 @@ remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_ad
 remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash' );
 
 function myPrice ($price) {
-    $price = str_replace(array('<ins>', '</ins>', '&euro;</span>&nbsp;', ',', ':'), array('', '', '&euro;</span>&nbsp;<strong>','</strong>,', ''), $price);
+    $price = str_replace(array('<ins>', '</ins>', '&euro;</span>&nbsp;', ',', ':'), array('', '', '&euro;</span>&nbsp;<span>','</span>,', ''), $price);
     return $price;
 }
 add_filter( 'woocommerce_get_price_html', 'myPrice' );
