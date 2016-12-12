d5c6573fe8acb6a8f12c4aa996ba11406d6d6db2
Mattia <guzzi.mattia@gmail.com>
2016-12-12 10:47:11 +0100

    no message

--- extras/woocommerce.php
+++ extras/woocommerce.php
@@ -6,4 +6,6 @@ function wcs_woo_remove_reviews_tab($tabs) {
     unset($tabs['reviews']);
     return $tabs;
 }
+
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart');
 ?>
