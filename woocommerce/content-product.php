110613d03a838f8e9127c32f9a6d3eb1ad3fd03b
Mattia <guzzi.mattia@gmail.com>
2016-12-12 10:43:27 +0100

    no message

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -50,8 +50,8 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 *
 	 * @hooked woocommerce_template_loop_product_title - 10
 	 */
-	do_action( 'woocommerce_shop_loop_item_title' );
-
+	/*do_action( 'woocommerce_shop_loop_item_title' );*/
+	?><div class="prova"></div><?php
 	/**
 	 * woocommerce_after_shop_loop_item_title hook.
 	 *
