5bc0c47c8b6d9b9f5280cdf3bf29b0e611df0536
Mattia <guzzi.mattia@gmail.com>
2016-12-12 12:29:29 +0100

    no message

--- woocommerce/single-product/price.php
+++ woocommerce/single-product/price.php
@@ -24,7 +24,11 @@ global $product;
 
 ?>
 <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-	<p class="price"><?php echo $product->get_price_html(); var_dump($product->get_price_html())?></p>
+	<p class="price"><?php $price = $product->get_price_html();
+		$price_split = explode(",", $price);
+		echo $price_split[0].", divisio";
+		echo $price_split[1].",";
+		?></p>
 
 	<meta itemprop="price" content="<?php echo esc_attr( $product->get_display_price() ); ?>" />
 	<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
