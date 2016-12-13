316bfd341150a8a5db01107b3f34f89397b5ed4c
Mattia <guzzi.mattia@gmail.com>
2016-12-13 16:02:59 +0100

    no message

--- extras/woocommerce.php
+++ extras/woocommerce.php
@@ -10,7 +10,7 @@ function wcs_woo_remove_reviews_tab($tabs) {
 remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart');
 remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash' );
 
-remove_action( 'woocommerce_result_count' , 'woocommerce_before_shop_loop' );
+remove_action( 'woocommerce_before_shop_loop' , 'woocommerce_result_count', 20 );
 
 function myPrice ($price) {
     $price = str_replace(array('<ins>', '</ins>', '&euro;</span>&nbsp;', ',', ':'), array('', '', '&euro;</span>&nbsp;<span class="int-price">','</span>,', ''), $price);
