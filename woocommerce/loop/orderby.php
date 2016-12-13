2ac2c2136b1e392436adc821d971bf4c5ca95d78
Mattia <guzzi.mattia@gmail.com>
2016-12-13 17:23:13 +0100

    no message

--- woocommerce/loop/orderby.php
+++ woocommerce/loop/orderby.php
@@ -10,63 +10,66 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
+ * @see        https://docs.woocommerce.com/document/template-structure/
+ * @author        WooThemes
+ * @package    WooCommerce/Templates
  * @version     2.2.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
 }
 
 ?>
-<form class="woocommerce-ordering" method="get">
-	<select name="orderby" class="orderby">
-		<?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
-			<option value="<?php echo esc_attr( $id ); ?>" <?php selected( $orderby, $id ); ?>><?php echo esc_html( $name ); ?></option>
-		<?php endforeach; ?>
-	</select>
-	<?php
-		// Keep query string vars intact
-		foreach ( $_GET as $key => $val ) {
-			if ( 'orderby' === $key || 'submit' === $key ) {
-				continue;
-			}
-			if ( is_array( $val ) ) {
-				foreach( $val as $innerVal ) {
-					echo '<input type="hidden" name="' . esc_attr( $key ) . '[]" value="' . esc_attr( $innerVal ) . '" />';
-				}
-			} else {
-				echo '<input type="hidden" name="' . esc_attr( $key ) . '" value="' . esc_attr( $val ) . '" />';
-			}
-		}
-	?>
-</form>
+    <form class="woocommerce-ordering" method="get">
+        <select name="orderby" class="orderby">
+            <?php foreach ($catalog_orderby_options as $id => $name) : ?>
+                <option
+                    value="<?php echo esc_attr($id); ?>" <?php selected($orderby, $id); ?>><?php echo esc_html($name); ?></option>
+            <?php endforeach; ?>
+        </select>
+        <?php
+        // Keep query string vars intact
+        foreach ($_GET as $key => $val) {
+            if ('orderby' === $key || 'submit' === $key) {
+                continue;
+            }
+            if (is_array($val)) {
+                foreach ($val as $innerVal) {
+                    echo '<input type="hidden" name="' . esc_attr($key) . '[]" value="' . esc_attr($innerVal) . '" />';
+                }
+            } else {
+                echo '<input type="hidden" name="' . esc_attr($key) . '" value="' . esc_attr($val) . '" />';
+            }
+        }
+        ?>
+    </form>
 
-<?php $args = array('taxonomy'  => 'product_cat');
+<?php $args = array('taxonomy' => 'product_cat');
 $terms = get_terms($args);
 if ($terms): ?>
-<div class="categories">
-	<a class="categories" href="#"><?php _e( 'Categorie' , 'jurgita' ) ?></a>
-<?php foreach ($terms as $term):
-	$query = new WP_Query(array(
-			'post_type' => "prodotti",
-			'posts_per_page' => -1,
-			'taxonomy'  => 'product_cat'
-		));
-		if ($query->have_posts()):?>
-			<ul>
-			<?php while ($query->have_posts()):
-				$query->the_post();
-				?>
-				<li>
-					<a href="<?php the_permalink(); ?> ">
-						<?php the_title(); ?>
-					</a>
-				</li>
-				<?php endwhile; wp_reset_query(); ?> ?>
-				</ul>
-</div>
-
-<? endif; ?>
+    <div class="categories">
+    <a class="categories" href="#"><?php _e('Categorie', 'jurgita') ?></a>
+    <?php foreach ($terms as $term):
+        $query = new WP_Query(array(
+            'post_type' => "prodotti",
+            'posts_per_page' => -1,
+            'taxonomy' => 'product_cat'
+        ));
+        if ($query->have_posts()):?>
+            <ul>
+                <?php while ($query->have_posts()):
+                    $query->the_post();
+                    ?>
+                    <li>
+                        <a href="<?php the_permalink(); ?> ">
+                            <?php the_title(); ?>
+                        </a>
+                    </li>
+                <?php endwhile;
+                wp_reset_query(); ?> ?>
+            </ul>
+        <?php endif; ?>
+        </div>
+    <?php endforeach; ?>
+<?php endif; ?>
