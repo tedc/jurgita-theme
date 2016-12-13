29b590ef46e100040dd4b948dba7b66c93152c22
Mattia <guzzi.mattia@gmail.com>
2016-12-13 10:58:13 +0100

    no message

--- woocommerce/product-searchform.php
+++ woocommerce/product-searchform.php
@@ -17,16 +17,19 @@
  */
 
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
 ?>
 
-<form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url( home_url( '/'  ) ); ?>">
-	<div class="container-search">
-	<input type="search" id="woocommerce-product-search-field" class="search-field" placeholder="<?php echo esc_attr_x( 'Search Products&hellip;', 'placeholder', 'woocommerce' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'woocommerce' ); ?>" />
-	</div>
-		<input class="btn" type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'woocommerce' ); ?>" />
-	<input type="hidden" name="post_type" value="product" />
+<form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url(home_url('/')); ?>">
+    <div class="container-search">
+        <input type="search" id="woocommerce-product-search-field" class="search-field"
+               placeholder="<?php _e('Cerca tra i prodotti', 'jurgita') ?>" value="<?php echo get_search_query(); ?>"
+               name="s" title="<?php echo esc_attr_x('Search for:', 'label', 'woocommerce'); ?>"/>
+    </div>
+    <input class="btn-reverse" type="submit"
+           value="<?php echo esc_attr_x('Search', 'submit button', 'woocommerce'); ?>"/>
+    <input type="hidden" name="post_type" value="product"/>
 </form>
