e776e032be9f02f4ef5f4bbc08aa144d2995dee2
Mattia <guzzi.mattia@gmail.com>
2016-12-14 11:21:07 +0100

    no message

--- builder/citazione.php
+++ builder/citazione.php
@@ -1,5 +1,5 @@
 <div class="row-quote">
 	<blockquote class="quote">
-		&ldquo;<?php the_field('frase'); var_dump(the_field('frase')) ?>&rdquo;
+		&ldquo;<?php the_sub_field('frase'); ?>&rdquo;
 	</blockquote>
 </div>
