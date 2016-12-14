4bf9d4204278ae010d4fa31f60a91b4d761d05b3
Mattia <guzzi.mattia@gmail.com>
2016-12-14 19:26:15 +0100

    no message

--- builder/prodotti.php
+++ builder/prodotti.php
@@ -12,3 +12,6 @@
     <?php wp_reset_query() ?>
     </ul>
 <?php } ?>
+<?php $shop = woocommerce_get_page_id( 'shop' ); ?>
+<a class="btn-header btn-shop" href="<?php echo get_permalink( $shop ); ?>" title="<?php echo get_the_title($shop); ?>"><span class="btn"><?php _e('Visita lo shop', 'jurgita'); ?></span></a>
+
