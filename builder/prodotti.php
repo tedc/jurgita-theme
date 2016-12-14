1387377bfa61012d6c677095df3899ea123c4db0
Mattia <guzzi.mattia@gmail.com>
2016-12-14 19:23:06 +0100

    no message

--- builder/prodotti.php
+++ builder/prodotti.php
@@ -1,9 +1,9 @@
 <h2 class="title aligncenter"><?php _e('Dal nostro shop', 'jurgita') ?></h2>
-<?php var_dump(get_sub_field('prodotti')); ?>
 <?php if ( get_sub_field('prodotti') ) { ?>
     <?php $query = new WP_Query(array(
         'post_type' => 'product',
-        'posts_per_page' => 4
+        'post__in' => get_sub_field('prodotti'),
+        'posts_per_page' => count(get_sub_field('prodotti'))
     )); ?>
     <?php while ($query -> have_posts()) : $query ->  the_post(); ?>
         <?php wc_get_template_part('content', 'product'); ?>
