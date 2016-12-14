374a919ff590d0ddc106c6a8dfc3cd5125f728ca
Mattia <guzzi.mattia@gmail.com>
2016-12-14 15:43:10 +0100

    no message

--- builder/mediacols.php
+++ builder/mediacols.php
@@ -1,5 +1,5 @@
-<?php echo have_rows('mediacols') ;?>
+<?php if (have_rows('mediacols')) { ;?>
     <div class="grid-4 grid-cols">
         <?php echo get_sub_field('cols') ?>
     </div>
-<?php ?>
+<?php } ?>
