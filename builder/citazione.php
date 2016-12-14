f4d94c49daa1664fe3d04470f5fb9493018328a0
Mattia <guzzi.mattia@gmail.com>
2016-12-14 16:33:59 +0100

    no message

--- builder/citazione.php
+++ builder/citazione.php
@@ -2,4 +2,10 @@
 	<blockquote class="quote">
 		&ldquo;<?php the_sub_field('frase'); ?>&rdquo;
 	</blockquote>
+	<div class="square-yellow-l">
+		<div class="square-black-l"></div>
+	</div>
+	<div class="square-yellow-r">
+		<div class="square-black-r"></div>
+	</div>
 </div>
