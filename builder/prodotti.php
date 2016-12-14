98932747601229ea62639e5b4098dcb4e2133c57
Mattia <guzzi.mattia@gmail.com>
2016-12-14 18:01:40 +0100

    no message

--- builder/prodotti.php
+++ builder/prodotti.php
@@ -1,12 +1,25 @@
 <h2 class="shop-title"><?php _e('Dal nostro shop', 'jurgita') ?></h2>
-<?php $posts = the_sub_field('prodotti'); ?>
-<?php if ($posts): ?>
-    <ul class="products grid-4">
-        <?php foreach ($posts as $post):  ?>
-            <li>
-                <a href="<?php the_permalink( $post->ID ); ?>"><?php echo get_the_title( $post->ID ); ?></a>
-                <span>Custom field from $post: <?php the_field('author', $post->ID); ?></span>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+<?php $posts = get_sub_field('prodotti'); ?>
+<?php $query = new WP_Query(array(
+    'post_type' => 'product',
+    'post__in' => $posts,
+    'posts_per_page' => -1
+)); ?>
+
+<?php if ($query->have_posts()): ?>
+
+
+    <?php woocommerce_product_loop_start(); ?>
+
+    <?php woocommerce_product_subcategories(); ?>
+
+    <?php while (have_posts()) : the_post(); ?>
+
+        <?php wc_get_template_part('content', 'product'); ?>
+
+    <?php endwhile; // end of the loop. ?>
+
+    <?php woocommerce_product_loop_end(); ?>
+
+
 <?php endif; ?>
