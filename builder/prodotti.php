4098f81ee2e7f51fb09ca7a84b08bc281590b9dc
Mattia <guzzi.mattia@gmail.com>
2016-12-14 18:34:34 +0100

    no message

--- builder/prodotti.php
+++ builder/prodotti.php
@@ -1,4 +1,4 @@
-<h2 class="shop-title"><?php _e('Dal nostro shop', 'jurgita') ?></h2>
+<h2 class="shop-title aligncenter"><?php _e('Dal nostro shop', 'jurgita') ?></h2>
 
 <?php $posts = get_sub_field('prodotti'); ?>
 <?php $query = new WP_Query(array(
@@ -9,16 +9,16 @@
 
 <?php if ($query -> have_posts()): ?>
 
+    <ul class="products grid-4">
 
-    <?php woocommerce_product_loop_start(); ?>
 
     <?php while ($query -> have_posts()) : $query -> the_post(); ?>
 
-        <?php get_template_part('woocommerce/content', 'product'); ?>
+        <?php wc_get_template_part('content', 'product'); ?>
 
-    <?php endwhile; wp_reset_query() ?>
+    <?php endwhile; wp_reset_postdata() ?>
 
-    <?php woocommerce_product_loop_end(); ?>
+  </ul>
 
 
 <?php endif; ?>
