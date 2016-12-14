f02cbb8bae91da133531c3cfff5af42d5da5e054
Mattia <guzzi.mattia@gmail.com>
2016-12-14 17:42:13 +0100

    no message

--- builder/prodotti.php
+++ builder/prodotti.php
@@ -3,9 +3,12 @@
 <?php if ($posts): ?>
     <ul class="products grid-4">
         <?php foreach ($posts as $post):  ?>
-            <li>
-                <a href="<?php the_permalink( $post->ID ); ?>">ciao</a>
-            </li>
+            <?php var_dump($post->ID) ?>
+            <?php var_dump(the_permalink( $post->ID )) ?>
+            <?php var_dump(the_title( $post->ID )) ?>
+         <!--   <li>
+                <a href="<?php /*the_permalink( $post->ID ); */?>"><?php /*the_title( $post->ID ); */?></a>
+            </li>-->
         <?php endforeach; ?>
     </ul>
 <?php endif; ?>
