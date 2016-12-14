5c7b295336bad63e643a91a98cd3c561c1371c0f
Mattia <guzzi.mattia@gmail.com>
2016-12-14 17:34:51 +0100

    no message

--- builder/prodotti.php
+++ builder/prodotti.php
@@ -1,12 +1,13 @@
 <h2 class="shop-title"><?php _e('Dal nostro shop', 'jurgita') ?></h2>
-<?php var_dump( get_field('prodotti')) ?>
-<?php $posts = get_field('prodotti'); ?>
-<?php if ($posts): ?>
+<?php $posts = the_sub_field('prodotti'); ?>
+<?php var_dump(the_sub_field('prodotti')) ?>
+<?php /*if ($posts): */?><!--
     <ul class="products grid-4">
-        <?php foreach ($posts as $post):  ?>
+        <?php /*foreach ($posts as $post):  */?>
             <li>
-                <a href="<?php the_permalink( $post->ID ); ?>"><?php echo get_the_title( $post->ID ); ?></a>
+                <a href="<?php /*the_permalink( $post->ID ); */?>"><?php /*echo get_the_title( $post->ID ); */?></a>
+                <span>Custom field from $post: <?php /*the_field('author', $post->ID); */?></span>
             </li>
-        <?php endforeach; ?>
+        <?php /*endforeach; */?>
     </ul>
-<?php endif; ?>
+--><?php /*endif; */?>
