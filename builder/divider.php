a8104cef7b21d01f9311e7fc118aa4b2feaa859c
Mattia <guzzi.mattia@gmail.com>
2016-12-15 18:55:06 +0100

    no message

--- builder/divider.php
+++ builder/divider.php
@@ -1,8 +1,8 @@
-<div class="divider">
+<div class="divider container">
     <div class="square-yellow"></div>
+    <div class="square-gray"></div>
     <a href="http://jurgita.dnative.com" class="logo">
         <span class="j">J</span>
         <span class="j">J</span>
     </a>
-    <div class="square-gray"></div>
 </div>
