7685aff82668313468551aea2852c6f4c3dfaf46
Mattia <guzzi.mattia@gmail.com>
2016-12-15 16:39:11 +0100

    no message

--- builder/divider.php
+++ builder/divider.php
@@ -1,2 +1,9 @@
 <div class="divider">
+    <div class="square-yellow">
+        <a href="http://jurgita.dnative.com" class="logo">
+            <span class="j">J</span>
+            <span class="j">J</span>
+        </a>
+        <div class="square-gray"></div>
+    </div>
 </div>
