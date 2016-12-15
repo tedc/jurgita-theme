0f15f783395906a250bf54c9dcc9da7928f22735
Mattia <guzzi.mattia@gmail.com>
2016-12-15 12:31:12 +0100

    no message

--- builder/attestati.php
+++ builder/attestati.php
@@ -1,17 +1,16 @@
 <h2 class="title aligncenter"><?php _e('I miei attestati', 'jurgita') ?></h2>
-
-
-<?php $query = new WP_Query(array(
-    "post_type" => "attestati",
-)); ?>
-<div class="carousel_attestati owl-theme">
-
+<div class="carousel_list">
+    <?php $query = new WP_Query(array(
+        "post_type" => "attestati",
+    )); ?>
     <?php while ($query->have_posts()) : $query->the_post();
-        $post = get_post(); ?>
-        <div class="item">
-
-        <?php the_post_thumbnail("large"); ?>
-
+       /* $post = get_post();*/ ?>
+        <div class="carousel_item">
+            <figure class="carousel_figure">
+                <a href="<?php the_permalink() ?>">
+                    <img src="<?php the_post_thumbnail_url("large"); ?>"></a>
+            </figure>
+            <h5><a class="m_button" href="<?php  the_permalink() ?>"><?php the_title() ?></a></h5>
         </div>
     <?php endwhile; ?>
 </div>
