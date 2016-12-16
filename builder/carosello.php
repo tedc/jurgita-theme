0f7959fb2aae11c64f4c217ff2cd50c6ab5a7479
Mattia <guzzi.mattia@gmail.com>
2016-12-16 11:30:55 +0100

    no message

---
+++ builder/carosello.php
@@ -0,0 +1,16 @@
+<div class="container box-carousel row-lg">
+    <div class="grid-4">
+        <div class="col-2 carousel">
+            <?php the_sub_field('carousel'); ?>
+        </div>
+        <div class="col-2 square-container">
+            <div class="square"></div>
+            <div class="square-shadow"></div>
+            <div class="gray-square"></div>
+            <a href="http://jurgita.dnative.com" class="logo">
+                <span class="j">J</span>
+                <span class="j">J</span>
+            </a>
+        </div>
+    </div>
+</div>
