2f84d3bfa6f514e90f2dac673f7f06988f1094b6
Mattia <guzzi.mattia@gmail.com>
2016-12-16 17:03:50 +0100

    no message

---
+++ builder/faq.php
@@ -0,0 +1,4 @@
+<div class="container row-lg faq">
+<h2 class="title text-center"><?php _e('Altre domande? Consulta le FAQ', 'jurgita')?></h2>
+<a class="btn-reverse" href="<?php the_sub_field('faq'); ?>"><?php _e('Leggi', 'jurgita')?></a>
+</div>
