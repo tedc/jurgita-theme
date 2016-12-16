f300cee20d5d31ca173120c6c3acc3bb7a5d1af5
tedc <egrandinetti@gmail.com>
2016-12-16 17:45:52 +0100

    btn shop

--- templates/header.php
+++ templates/header.php
@@ -6,7 +6,7 @@
 	</a>
 	<div class="menu-container">
 		<?php icl_selector(); ?>
-		<a class="btn-header btn-shop" href="<?php echo get_permalink( $shop ); ?>" title="<?php echo get_the_title($shop); ?>"><span class="btn-header-text"><?php _e('Shop', 'jurgita'); ?></span><?php woocommerce_car_icon(); ?></a>
+		<span class="btn-header btn-shop"><a href="<?php echo get_permalink( $shop ); ?>" title="<?php echo get_the_title($shop); ?>" class="btn-header-text"><?php _e('Shop', 'jurgita'); ?></a><?php woocommerce_car_icon(); ?></span>
 		<?php wp_nav_menu( ['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container_id' => 'menu', 'container_class' => 'menu'] ); ?>
 		<a class="btn-header btn-menu" href="#" data-reveal="#menu">
 			<span class="btn-header-text"><?php _e('Menu', 'jurgita'); ?></span>
