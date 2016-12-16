994de147e7c7652311d37e51d8a932b92a3a0bae
Mattia <guzzi.mattia@gmail.com>
2016-12-16 12:50:02 +0100

    no message

--- builder/immagine.php
+++ builder/immagine.php
@@ -1,2 +1,2 @@
 <?php $img = get_sub_field('immagine'); ?>
-<img src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>" class="row-img" />
+<img src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>" class="row-img full-img" />
