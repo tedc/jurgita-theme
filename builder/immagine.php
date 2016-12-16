1f9af3219681302f67c31bb84f66c1c693934e2e
Mattia <guzzi.mattia@gmail.com>
2016-12-16 15:14:33 +0100

    no message

--- builder/immagine.php
+++ builder/immagine.php
@@ -1,2 +1,12 @@
 <?php $img = get_sub_field('immagine'); ?>
-<img src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>" class="row-img full-img" />
+<div class="container-img">
+    <img src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>" class="row-img full-img"/>
+    <?php $random = rand(0, 2);?>
+    <?php if ($rand == 0) {
+    } ?>
+    <?php if ($rand == 1){?>
+    <?php echo '<div class="square"></div><div class="square-shadow"></div>' ?>
+    <?php } else { ?>
+    <?php echo '<div class="square-shadow"></div>'?>
+    <?php } ?>
+</div>
