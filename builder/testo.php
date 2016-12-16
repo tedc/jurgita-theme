730697eb52dd514809861292746d769ce5901731
Mattia <guzzi.mattia@gmail.com>
2016-12-16 11:10:48 +0100

    no message

--- builder/testo.php
+++ builder/testo.php
@@ -1,8 +1,8 @@
 <section class="container row-lg">
 	<div class="row-content content">
 	<?php if( get_sub_field('titolo') ) : ?>
-	<h2 class="title"><?php the_field('titolo'); ?></h2>
+	<h2 class="title"><?php the_sub_field('titolo'); ?></h2>
 	<?php endif; ?>
-	<?php the_field('contenuto'); ?>
+	<?php the_sub_field('contenuto'); ?>
 	</div>
 </section>
