2a9cf4600cc51d357738e2694698cc9759e28007
Mattia <guzzi.mattia@gmail.com>
2016-12-17 15:05:01 +0100

    no message

--- builder/instagram.php
+++ builder/instagram.php
@@ -1,8 +1,11 @@
 <div class="container instagram row-lg">
     <h2 class="title aligncenter"><?php _e('Jurgita su Instagram', 'jurgita') ?></h2>
-    <?php echo do_shortcode('[instagram-feed]') ?>
-    <a href="<?php bloginfo('url'); ?>" class="logo">
-        <span class="j">J</span>
-        <span class="j">J</span>
-    </a>
+    <div class="container-img">
+        <?php echo do_shortcode('[instagram-feed]') ?>
+        <a href="<?php bloginfo('url'); ?>" class="logo">
+            <span class="j">J</span>
+            <span class="j">J</span>
+        </a>
+    </div>
+    <a href="https://www.instagram.com/rimastino/" class="btn" target="_blank"><?php _e('Segui', 'jurgita') ?></a>
 </div>
