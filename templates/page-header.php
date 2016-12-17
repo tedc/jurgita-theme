c984874e66448167b838cc957aa1fe4bd83b1ce9
tedc <egrandinetti@gmail.com>
2016-12-17 11:10:33 +0100

    home header

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,27 @@
 <?php use Roots\Sage\Titles; ?>
+<header class="page-header">
+	<?php if(!is_front_page()) : ?>
+	<h1 class="title"><?= Titles\title(); ?></h1>
+	<?php get_field('sottotitolo') : ?>
+	<h2 class="subtitle"><?php the_field('sottotitolo'); ?></h2>
+	<?php endif; ?>
+	<?php else : ?>
+	<?php 
+		$name = explode(' ', get_bloginfo('name'));
+		$description explode('&', get_bloginfo( 'description') );
+		$about = get_field('about');
+		$i = 1;
+		$h1 = '<h1 class="title">'
+		foreach ($name as $n) {
+			$h1 .= ($n < count($name)) ? $n . '<br>' : ? $n . '</h1>';
+			$i++;
+		}
+		echo $h1;
+		echo '<h2 class="subtitle">' .$description[0].'<br/>&'.$description[1].'</h2>';
+		if($about) :
+			echo '<a class="btn-reverse" href="'.get_permalink($about).'">'.get_the_title( $about ).'</a>';
+		endif;
+	?>
 
-<div class="page-header">
-  <h1><?= Titles\title(); ?></h1>
-</div>
+	<?php endif; ?>
+</header>
