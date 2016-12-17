7be279892a67c0cdbe6715290039f981d9f02d27
tedc <egrandinetti@gmail.com>
2016-12-17 13:39:27 +0100

    image header

--- templates/menu-footer.php
+++ templates/menu-footer.php
@@ -1,7 +1,7 @@
 <footer class="menu-footer">
 	<nav classs="menu-footer-social">
-		<a class="icon-linkedin" href="<?php echo get_option("wpseo_social")["linkedin_url"] ?>"></a>
 		<a class="icon-facebook" href="<?php echo get_option("wpseo_social")["facebook_site"] ?>"></a>
+		<a class="icon-instagram" href="<?php echo get_option("wpseo_social")["instagram_url"] ?>"></a>
 	</nav>
 	<div class="menu-footer-phone">
 		<?php _e('Tel', 'jurgita'); ?>: <?php acf_set_language_to_default(); ?><a href="tel:<?php echo get_field('telefono', 'options'); ?>"><?php the_field('telefono', 'options'); ?></a><?php acf_unset_language_to_default(); ?>
