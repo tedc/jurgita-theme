0af7903dd2f27ffcd89072a62265c22aaf39d08d
Mattia <guzzi.mattia@gmail.com>
2016-12-17 14:53:40 +0100

    no message

--- builder/instagram.php
+++ builder/instagram.php
@@ -1,4 +1,8 @@
 <div class="container instagram row-lg">
     <h2 class="title aligncenter"><?php _e('Jurgita su Instagram', 'jurgita') ?></h2>
     <?php echo do_shortcode('[instagram-feed]') ?>
+    <a href="<?php bloginfo('url'); ?>" class="logo">
+        <span class="j">J</span>
+        <span class="j">J</span>
+    </a>
 </div>
