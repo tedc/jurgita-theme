90d2ba4c83481768f0967050f318ca263db6abf0
tedc <egrandinetti@gmail.com>
2016-12-17 15:30:17 +0100

    page custom

--- template-custom.php
+++ template-custom.php
@@ -3,8 +3,8 @@
  * Template Name: Custom Template
  */
 ?>
-
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+  <?php get_template_part('templates/page', 'layout'); ?>
 <?php endwhile; ?>
+
