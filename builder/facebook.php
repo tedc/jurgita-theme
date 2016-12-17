8996a73add36bbecd8f906747720d98b71102feb
Mattia <guzzi.mattia@gmail.com>
2016-12-17 11:55:49 +0100

    no message

---
+++ builder/facebook.php
@@ -0,0 +1,16 @@
+<div class="container facebook">
+    <span class="pattern"></span>
+    <span class="square"></span>
+    <div class="like-content">
+        <div class="gray-square"></div>
+        <span class="like"></span>
+        <a href="<?php bloginfo('url'); ?>" class="logo">
+            <span class="j">J</span>
+            <span class="j">J</span>
+        </a>
+        <a class="btn"><?php _e('Diventa fan', 'jurgita') ?></a>
+    </div>
+    <figure>
+        <img>
+    </figure>
+</div>
