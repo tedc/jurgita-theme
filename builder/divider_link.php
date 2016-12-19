a84d96aadfdd031f04114a4c098714ca1d95113f
Mattia <guzzi.mattia@gmail.com>
2016-12-19 12:43:57 +0100

    no message

--- builder/divider_link.php
+++ builder/divider_link.php
@@ -1,4 +1,6 @@
 <div class="divider-link container">
+    <div class="gray-line"></div>
+    <div class="shadow"></div>
     <div class="square-gray"></div>
     <div class="square-yellow">
         <a href="#"><?php _e('Calendario dei corsi', 'jurgita') ?></a>
@@ -7,7 +9,4 @@
         <span class="j">J</span>
         <span class="j">J</span>
     </a>
-    <div class="gray-line"></div>
-    <div class="shadow"></div>
-
 </div>
