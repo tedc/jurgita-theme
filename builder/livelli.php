af0e7d204f14c3ffbc305453f1ce9be061731cff
Mattia <guzzi.mattia@gmail.com>
2016-12-19 10:53:11 +0100

    no message

--- builder/livelli.php
+++ builder/livelli.php
@@ -5,24 +5,24 @@ if ($terms): ?>
 
     <div class="grid-4">
 
-    <?php foreach ($terms as $term): ?>
-        <div class="col-1">
-        <h2><?php echo $term->name; ?></h2>
-            <?php
+        <?php foreach ($terms as $term): ?>
+            <div class="col-1">
+                <h2><?php echo $term->name; ?></h2>
+                <?php
 
-            // check if the repeater field has rows of data
-            if( have_rows('argomenti') ):
+                // check if the repeater field has rows of data
+                if (have_rows('argomenti')):
 
-            // loop through the rows of data
-            while ( have_rows('argomenti') ) : the_row();
+                    // loop through the rows of data
+                    while (have_rows('argomenti')) : the_row();
 
-                // display a sub field value
-                the_sub_field('argomento');
-                the_sub_field('check');
+                        // display a sub field value
+                        the_sub_field('argomento');
+                        the_sub_field('check');
 
-            endwhile;?>
-            <?php endif ?>
-
-            <?php endforeach; ?>
-        </div>
-        <?php endif; ?>
+                    endwhile; ?>
+                <?php endif ?>
+            </div>
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
