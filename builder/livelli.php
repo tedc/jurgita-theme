e4e3fc0655ea102eada5ca72fbc0b001695a46c9
Mattia <guzzi.mattia@gmail.com>
2016-12-19 11:19:34 +0100

    no message

--- builder/livelli.php
+++ builder/livelli.php
@@ -8,14 +8,12 @@ if ($terms): ?>
         <?php foreach ($terms as $term): ?>
             <div class="col-1">
                 <h2><?php echo $term->name; ?></h2>
-                <?php if( have_rows('repeater_field_name') ):
-                while (have_rows('repeater_field_name') ) : the_row();
 
-                    // display a sub field value
-                    $term -> the_sub_field('argomento');
 
-                endwhile;?>
-                <?php endif; ?>
+                
+                    <?php $term -> the_sub_field('argomento'); ?>
+
+
                 </div>
         <?php endforeach; ?>
     </div>
