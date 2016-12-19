7e614e9a4506c69ab1ce238c56555c1847930cd4
Mattia <guzzi.mattia@gmail.com>
2016-12-19 12:28:50 +0100

    no message

--- builder/livelli.php
+++ builder/livelli.php
@@ -8,7 +8,6 @@ if ($terms): ?>
         <?php foreach ($terms as $term): ?>
             <div class="col-1">
                 <h2><?php echo $term->name; ?></h2>
-                <?php var_dump($term->taxonomy.'_'.$term->term_id) ?>
                 </div>
         <?php endforeach; ?>
     </div>
