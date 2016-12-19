42d2ba279488eb8c42402a3e422e954db4906936
tedc <egrandinetti@gmail.com>
2016-12-19 18:41:21 +0100

    checkbox

--- extras/utils.php
+++ extras/utils.php
@@ -1,7 +1,7 @@
 <?php
 	function unwrap_cf_checkbox($content) {
-		$reg = '/<span class="(.*?)acceptance">(<input(.*?)type="checkbox"(.*?)>)<\/span>';
-		$content = preg_replace($reg, "$2", $content);
+		$reg = '/<span class="(.*?)acceptance">(.*?)(<input(.*?)type="checkbox"(.*?)>)(.*?)<\/span>';
+		$content = preg_replace($reg, "$3", $content);
 		return $content;
 	}
 	add_filter('wpcf7_form_elements', 'unwrap_cf_checkbox');
