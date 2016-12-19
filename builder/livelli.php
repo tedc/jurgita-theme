8824adf2982316a237ee125651caeaf89978d77e
Mattia <guzzi.mattia@gmail.com>
2016-12-19 11:03:55 +0100

    no message

--- builder/livelli.php
+++ builder/livelli.php
@@ -8,14 +8,26 @@ if ($terms): ?>
         <?php foreach ($terms as $term): ?>
             <div class="col-1">
                 <h2><?php echo $term->name; ?></h2>
+
+                <?php $args = array(
+                    'post_type' => "corsi",
+                    'posts_per_page' => -1,
+                    'tax_query' => array(
+                        array(
+                            'taxonomy' => 'livelli',
+                            'field' => 'name',
+                            'terms' => $term->name
+                        )
+                    )
+                );
+                $query = new WP_Query($args);?>
                 <?php
+                if ($query -> have_rows('argomenti')):
 
-                if ($term->have_rows('argomenti')):
+                    while ($query -> have_rows('argomenti')) : $query -> the_row();
 
-                    while ($term->have_rows('argomenti')) : $term->the_row();
-
-                        $term->the_sub_field('argomento');
-                        $term->the_sub_field('check');
+                        $query->the_sub_field('argomento');
+                        $query->the_sub_field('check');
 
                     endwhile; ?>
                 <?php endif ?>
