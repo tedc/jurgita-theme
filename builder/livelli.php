7da8e6ac2b2a6f715cba465a7c8c9587f8ead23b
Mattia <guzzi.mattia@gmail.com>
2016-12-19 10:12:46 +0100

    no message

--- builder/livelli.php
+++ builder/livelli.php
@@ -1,10 +1,19 @@
 <?php
 
 $term = get_sub_field('tipologie');
+var_dump(get_sub_field('tipologie'));
+if( $terms ): ?>
 
-if( $term ): ?>
+    <ul>
 
-    <h2><?php echo $term->name; ?></h2>
-    <p><?php echo $term->description; ?></p>
+        <?php foreach( $terms as $term ): ?>
+
+            <h2><?php echo $term->name; ?></h2>
+            <p><?php echo $term->description; ?></p>
+            <a href="<?php echo get_term_link( $term ); ?>">View all '<?php echo $term->name; ?>' posts</a>
+
+        <?php endforeach; ?>
+
+    </ul>
 
 <?php endif; ?>
