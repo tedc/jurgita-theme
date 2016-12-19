a04bd0ab2c5a87347be47204cb8de53c14443b09
tedc <egrandinetti@gmail.com>
2016-12-19 18:42:04 +0100

    regex fix

--- extras/utils.php
+++ extras/utils.php
@@ -1,6 +1,6 @@
 <?php
 	function unwrap_cf_checkbox($content) {
-		$reg = '/<span class="(.*?)acceptance">(.*?)(<input(.*?)type="checkbox"(.*?)>)(.*?)<\/span>';
+		$reg = '/<span class="(.*?)acceptance">(.*?)(<input(.*?)type="checkbox"(.*?)>)(.*?)<\/span>/';
 		$content = preg_replace($reg, "$3", $content);
 		return $content;
 	}
