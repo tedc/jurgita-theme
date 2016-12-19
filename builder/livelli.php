f7fe599cac068b396c4899eb764aeef3829fc39f
Mattia <guzzi.mattia@gmail.com>
2016-12-19 12:04:24 +0100

    no message

--- builder/livelli.php
+++ builder/livelli.php
@@ -8,8 +8,9 @@ if ($terms): ?>
         <?php foreach ($terms as $term): ?>
             <div class="col-1">
                 <h2><?php echo $term->name; ?></h2>
+                <?php var_dump($term->taxonomy.'_'.$term->term_id) ?>
                 <?php var_dump(the_sub_field('argomento', $term->taxonomy.'_'.$term->term_id));?>
-                
+
                 </div>
         <?php endforeach; ?>
     </div>
