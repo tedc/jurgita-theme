03db4ef09f1cddaed3960d5bda5bc047f811f66c
tedc <egrandinetti@gmail.com>
2016-12-20 10:16:05 +0100

    contatti

--- template-contatti.php
+++ template-contatti.php
@@ -4,11 +4,11 @@
  */
 ?>
 <?php while (have_posts()) : the_post(); ?>
-	<article <?php post_class("container row-lg body-contentainer"); ?>>
+	<article <?php post_class("container row-lg body-container"); ?>>
 		<header class="contact-header">
 			<h1 class="title"><?php _e('Scrivimi', 'jurgita'); ?></h1>
 			<div class="right">
-			<span class="whatsapp"><?php _e('anche su', 'jurgita'); ?><?php acf_set_language_to_default(); ?><a href="whatsapp://send?abid=<?php the_field('whatsapp', 'options'); ?>&text=<?php _e('Ciao Jurgita', 'jurgita'); ?>" class="whatsapp-link">WhatsApp</a><a href="tel:<?php echo get_field('telefono', 'options'); ?>"><?php the_field('telefono', 'options'); ?></a><?php acf_unset_language_to_default(); ?></span>
+			<span class="whatsapp"><?php _e('anche su', 'jurgita'); ?><?php acf_set_language_to_default(); ?><a href="whatsapp://send?abid=<?php the_field('whatsapp', 'options'); ?>&text=<?php _e('Ciao Jurgita', 'jurgita'); ?>" class="whatsapp-link">WhatsApp</a></span><a href="tel:<?php echo get_field('telefono', 'options'); ?>"><?php the_field('telefono', 'options'); ?></a><?php acf_unset_language_to_default(); ?></div>
 		</header>
 		<?php get_template_part('templates/content', 'page'); ?>
 	</article>
