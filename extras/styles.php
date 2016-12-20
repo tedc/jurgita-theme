91e5e0437e2255a4deb8060f78b5e381d030d64e
tedc <egrandinetti@gmail.com>
2016-12-20 10:32:04 +0100

    clean css

--- extras/styles.php
+++ extras/styles.php
@@ -1,3 +1,7 @@
 <?php
-add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
-?>
+
+	add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+	function clean_css() {
+		wp_deregister_style( 'contact-form-7' );
+	}
+	add_action( 'init', 'clean_css');
