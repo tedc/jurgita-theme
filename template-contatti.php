7c0f79d315eb45815b5d42ae4b31ae345097170d
Mattia <guzzi.mattia@gmail.com>
2016-12-20 10:05:13 +0100

    no message

--- template-contatti.php
+++ template-contatti.php
@@ -4,7 +4,10 @@
  */
 ?>
 
+
 <?php while (have_posts()) : the_post(); ?>
-	<h1 class="title"><?php _e('Scrivici', 'jurgita'); ?></h1>
-	<?php get_template_part('templates/content', 'page'); ?>
+    <div class="container scrivimi">
+        <h1 class="title"><?php _e('Scrivimi', 'jurgita'); ?></h1>
+        <?php get_template_part('templates/content', 'page'); ?>
+    </div>
 <?php endwhile; ?>
