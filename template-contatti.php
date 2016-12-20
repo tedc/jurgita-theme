12124d916fa476ef8f8db708b35184241a68d5a0
Mattia <guzzi.mattia@gmail.com>
2016-12-20 10:10:18 +0100

    no message

--- template-contatti.php
+++ template-contatti.php
@@ -8,7 +8,7 @@
 <?php while (have_posts()) : the_post(); ?>
 	<article <?php post_class("body-container row-lg content"); ?>>
 		<header class="contact-header">
-			<h1 class="title"><?php _e('Scrivici', 'jurgita'); ?></h1>
+			<h1 class="title"><?php _e('Scrivimi', 'jurgita'); ?></h1>
 			<div class="right">
 			<span class="whatsapp"><?php _e('anche su', 'jurgita'); ?><?php acf_set_language_to_default(); ?><a href="whatsapp://send?abid=<?php the_field('whatsapp', 'options'); ?>&text=<?php _e('Ciao Jurgita', 'jurgita'); ?>" class="whatsapp-link">WhatsApp</a><a href="tel:<?php echo get_field('telefono', 'options'); ?>"><?php the_field('telefono', 'options'); ?></a><?php acf_unset_language_to_default(); ?></span>
 		</header>
