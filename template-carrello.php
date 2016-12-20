2739776ead02fa22dad45e8b2832a85eba5411a5
Mattia <guzzi.mattia@gmail.com>
2016-12-20 16:19:05 +0100

    no message

--- template-carrello.php
+++ template-carrello.php
@@ -5,7 +5,6 @@
 ?>
 <?php use Roots\Sage\Titles; ?>
 
-<?php get_template_part('templates/page', 'header'); ?>
 <?php get_template_part('templates/breadcrumbs'); ?>
 <h1 class="title"><?= Titles\title(); ?></h1>
 <?php get_template_part('templates/content', 'page'); ?>
