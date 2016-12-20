9433034e92b7f2eca6d9aed28a341383993927ba
Mattia <guzzi.mattia@gmail.com>
2016-12-20 18:56:36 +0100

    no message

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -144,7 +144,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 					</div>
 				<?php } ?>
 
-				<input type="submit" class="btn button" name="update_cart" value="<?php esc_attr_e( 'Update Cart', 'woocommerce' ); ?>" />
+				<input type="submit" class="btn-reverse button" name="update_cart" value="<?php esc_attr_e( 'Update Cart', 'woocommerce' ); ?>" />
 
 				<?php do_action( 'woocommerce_cart_actions' ); ?>
 
