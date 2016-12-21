a393fe9a2482d4d35acb7a2d9edf39c43c1a0974
tedc <egrandinetti@gmail.com>
2016-12-21 13:00:14 +0100

    panel

--- templates/panel.php
+++ templates/panel.php
@@ -31,24 +31,34 @@ $corsi = new WP_Query(array(
 )); if($corsi->have_posts()) : ?>
     <aside class="aside">
         <div class="panel" id="panel">
-            <a href="#" data-revael="#panel">
-                <ul>
-                    <?php
-                    while($corsi->have_posts()) : $corsi->the_post();
-                        // make date object
-                        $start = new DateTime(
-                            get_field('data_inizio', false, false)
-                        );
-                        // make date object
-                        $end = new DateTime(
-                            get_field('data_fine', false, false)
-                        );
-                        ?>
-                        <li>
-                        </li>
-                    <?php endwhile; wp_reset_query(); ?>
-                </ul>
+            <ul>
+                <?php 
+                    $today = date('Ymd');
+                    $args = array (
+                        'post_type' => 'corsi',
+                        'meta_query' => array(
+                            array(
+                                'key'       => 'data_inizio',
+                                'compare'   => '>=',
+                                'value'     => $today,
+                            ),
+                             array(
+                                'key'       => 'data_fine',
+                                'compare'   => '>=',
+                                'value'     => $today,
+                            )
+                        ),
+                        'meta_key'  => 'data_fine',
+                        'orderby'   => 'meta_value_num',
+                        'order'     => 'ASC'
+                    );
+                    $query = new WP_Query($args); ?>
+                    <?php if ($query->have_posts()) : $i = 0; while ($query->have_posts()) : $query->the_post(); ?>
+                    <li <?php post_class(); ?>>
+                    </li>
+                <?php endwhile; wp_reset_query(); ?>
+            </ul>
         </div>
-        <a href="#" class="aside-btn"><?php _e('Prossimi corsi', 'jurgita'); ?><i class="icon-arrow-down"></i></a>
+        <a href="#" class="aside-btn" data-reveal="#panel"><?php _e('Prossimi corsi', 'jurgita'); ?><i class="icon-arrow-down"></i></a>
     </aside>
 <?php endif; ?>
