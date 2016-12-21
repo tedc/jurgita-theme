e8d4079110758b50f70530cbe2919ee46dbe334e
tedc <egrandinetti@gmail.com>
2016-12-21 11:27:52 +0100

    corsi

--- extras/utils.php
+++ extras/utils.php
@@ -11,11 +11,11 @@
 		$end = new DateTime($e);
 		$date = '<span class="corsi-date">';
 		if($start->format('F') === $end->format('F') && $start->format('Y') === $end->format('Y')) :
-			$date .= '<span class="day-group">'.$start->format('d') . '<span class="sep">/</span>' . $end->format('d') . '</span><span class="bottom-date">'.date_i18n('F', strtotime($s)).'<span class="year">&nbsp;'.$start->format('Y').'</span>';
+			$date .= '<span class="day-group">'.$start->format('d') . '<span class="sep">/</span>' . $end->format('d') . '</span><span class="bottom-date">'.date_i18n('F', strtotime($s)).'<span class="year">'.$start->format('Y').'</span>';
 		else :
 			$date .= '<span class="corsi-date-row">'.$start->format('d') . '<span class="month">' . date_i18n('F', strtotime( $s ) ) . '</span>';
 			$date .= ($start->format('Y') != $end->format('Y')) ? '<span class="year">&nbsp;'.$start->format('Y').'</span></span>' : '</span>';
-			$date .= '<span class="corsi-date-row">'.$end->format('d') . '<span class="month">' . date_i18n('F', strtotime( $e ) ) . '</span><span class="year">&nbsp;'.$end->format('Y').'</span></span>';
+			$date .= '<span class="corsi-date-row">'.$end->format('d') . '<span class="month">' . date_i18n('F', strtotime( $e ) ) . '</span><span class="year">'.$end->format('Y').'</span></span>';
 		endif;
 		$date .= '</span>';
 		echo $date;
