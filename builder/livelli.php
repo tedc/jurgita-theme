8872dd7c816078f81e4f7ac1760ca2da7e298058
tedc <egrandinetti@gmail.com>
2016-12-21 16:45:09 +0100

    livelli

--- builder/livelli.php
+++ builder/livelli.php
@@ -8,18 +8,15 @@ if ($terms): ?>
             <div class="col-2">
                 <h2 class="title title-corso"><?php echo $term->name; ?></h2>
                 <ul>
-                    <?php while (have_rows('argomenti', 'livelli_' . $term->term_id)) : the_row();
-                        if (the_sub_field('check')) {
-                            echo '<li class="argomento">';
-                            echo '<span class="icon icon-check">';
-                            echo '<span class="argomento-text">' . the_sub_field('argomento') . '</span>';
-                            echo '</li>';
-                        } else {
-                            echo '<li class="argomento">';
-                            echo '<span class="icon">';
-                            echo '</li>';
-                        }
-                    endwhile; ?>
+                    <?php while (have_rows('argomenti', 'livelli_' . $term->term_id)) : the_row(); ?>
+                        <li class="argomento">
+                            <span class="check">
+                                <?php echo (get_sub_field('check')) ? '<i class="icon-check"></i>' : ''; ?>
+                            </span>
+                            <?php echo (get_sub_field('check')) ? '<span class="argomento-text">'.get_sub_field('argomento').'</span>' : ''; ?>
+                        </li>
+                  <?php  endwhile; ?>
+                  </ul>
             </div>
         <?php endforeach; ?>
     </div>
