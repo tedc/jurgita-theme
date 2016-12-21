f58fd195de2bd6d85370292cda1697d09b84789d
Mattia <guzzi.mattia@gmail.com>
2016-12-21 15:35:56 +0100

    no message

--- builder/livelli.php
+++ builder/livelli.php
@@ -4,11 +4,19 @@ $terms = get_sub_field('tipologie');
 if ($terms): ?>
 
     <div class="grid-4">
-
         <?php foreach ($terms as $term): ?>
             <div class="col-1">
                 <h2><?php echo $term->name; ?></h2>
-                </div>
+                <?php while ( have_rows('argomenti', 'livelli_'.$term->term_id ) ) : the_row();
+
+                // display a sub field value
+                the_sub_field('argomento');
+                the_sub_field('check');
+
+
+                endwhile;
+                ?>
+            </div>
         <?php endforeach; ?>
     </div>
 <?php endif; ?>
