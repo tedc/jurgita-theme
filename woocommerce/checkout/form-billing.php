3124ee46c2f8b190514aaf510fb5e18beb2b7b89
Mattia <guzzi.mattia@gmail.com>
2016-12-22 12:09:00 +0100

    no message

--- woocommerce/checkout/form-billing.php
+++ woocommerce/checkout/form-billing.php
@@ -30,7 +30,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<?php else : ?>
 
-		<h3><?php _e( 'Billing Details', 'woocommerce' ); ?></h3>
+		<h3 class="title"><?php _e( 'Billing Details', 'woocommerce' ); ?></h3>
 
 	<?php endif; ?>
 
