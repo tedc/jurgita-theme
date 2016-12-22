b6c2000ebe88b0c7ac1e4e48bbc55a151d77adaf
Mattia <guzzi.mattia@gmail.com>
2016-12-22 17:54:37 +0100

    no message

--- extras/setup.php
+++ extras/setup.php
@@ -158,3 +158,8 @@ function facebook_at_init() {
 }
 						  
 add_action('init', 'facebook_at_init');
+
+
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
