6b03d0befe3d35a579f44cce0fecb30685d73ac5
Mattia <guzzi.mattia@gmail.com>
2016-12-28 12:08:17 +0100

    no message

--- builder/programma.php
+++ builder/programma.php
@@ -1,17 +1,16 @@
-<h2 class="title"><?php the_sub_field('titolo')?></h2>
-<?php if( have_rows('giorni') ): ?>
-<div class="grid-4 container programma-corsi">
-    <?php while ( have_rows('giorni') ) : the_row(); ?>
-        <h2 class="title title-corso"><?php the_sub_field('titolo'); ?></h2>
-        <div class="col-2">
-            <ul class="argomenti">
-        <?php while ( have_rows('argomenti') ) : the_row(); ?>
-                <li class="argomento">
-                            <span class="check"> </span>
-           <span class="argomento-text"> <?php the_sub_field('argomento'); ?></span>
-        <?php endwhile ?>
+<h2 class="title"><?php the_sub_field('titolo') ?></h2>
+<?php if (have_rows('giorni')): ?>
+    <div class="grid-4 container programma-corsi">
+        <?php while (have_rows('giorni')) : the_row(); ?>
+            <h2 class="title title-corso"><?php the_sub_field('titolo'); ?></h2>
+                <ul class="argomenti">
+                    <?php while (have_rows('argomenti')) : the_row(); ?>
+                        <li class="argomento col-2">
+                            <span class="check"></span>
+                            <span class="argomento-text"> <?php the_sub_field('argomento'); ?></span>
+                        </li>
+                    <?php endwhile ?>
                 </ul>
-        </div>
-    <?php endwhile ?>
-</div>
+        <?php endwhile ?>
+    </div>
 <?php endif ?>
