5a684d5a65000933e05550a717d7b8e58dba978e
tedc <egrandinetti@gmail.com>
2016-12-28 16:51:23 +0100

    uploads

--- builder/divider_link.php
+++ builder/divider_link.php
@@ -4,6 +4,7 @@
     <div class="square-gray" data-scrollmagic='{"tween":[{"y": -50, "x" : 50}, {"y" : 0, "x" : 0}], "triggerHook" : 0.5, "duration" : 0, "triggerElement": "#row_<?php echo $row; ?>"}'></div>
     <div class="square-yellow" data-scrollmagic='{"tween":[{"y": 50, "x" : 50}, {"y" : 0, "x" : 0}], "triggerHook" : 0.5, "duration" : 0, "triggerElement": "#row_<?php echo $row; ?>"}'>
         <a href="<?php the_field('link_corsi', 'options'); ?>"><?php _e('Calendario<br>dei corsi', 'jurgita') ?></a>
+        <a href="<?php the_field('link_corsi', 'options'); ?>" class="square-yellow-permalink"><?php _e('Calendario<br>dei corsi', 'jurgita') ?></a>
     </div>
     <a href="<?php bloginfo('url'); ?>" class="logo" data-scrollmagic='{"tween":[{"y": -50, "x" : -50}, {"y" : 0, "x" : 0}], "triggerHook" : 0.5, "duration" : 0, "triggerElement": "#row_<?php echo $row; ?>"}'>
         <span class="j">J</span>
