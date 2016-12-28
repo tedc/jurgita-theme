8d4355b94905b8c718b3fd1ba6de0ea93e842825
Mattia <guzzi.mattia@gmail.com>
2016-12-28 11:43:12 +0100

    no message

--- builder/programma.php
+++ builder/programma.php
@@ -2,10 +2,10 @@
 <?php if( have_rows('giorni') ): ?>
 <div class="grid-4 container programma-corsi">
     <?php while ( have_rows('giorni') ) : the_row(); ?>
-    <div class="col-2">
-    <?php the_sub_field('titolo'); ?>
-        
-    </div>
+        <h2 class="title title-corso"><?php the_sub_field('titolo'); ?></h2>
+        <div class="col-2">
+
+        </div>
     <?php endwhile ?>
 </div>
 <?php endif ?>
