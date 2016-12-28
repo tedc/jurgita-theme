9f40e7ba062a93bd1741fd33c2994056cdc03915
tedc <egrandinetti@gmail.com>
2016-12-28 17:28:02 +0100

    "triggerHook" : "#master"

--- builder/colonne.php
+++ builder/colonne.php
@@ -37,11 +37,11 @@ $dataScrollMagic = ($col%2==0) ? 'data-scrollmagic=\'{"tween":[{"y": 50, "x" : -
 </div>
 <?php elseif (get_row_layout() == 'master') : ?>
 	<div class="container master" id="master">
-		<span class="pattern" data-scrollmagic='{"tween":[{"y": 50, "x" : -50, "opacity" : 0}, {"y" : 0, "x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0, "triggerHook" : "#master"}'></span>
-		<span class="square" data-scrollmagic='{"tween":[{"y": 50, "opacity" : 0}, {"y" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0, "triggerHook" : "#master"}'></span>
-		<div class="master-content" data-scrollmagic='{"tween":[{"y": 50, "x" : 50, "opacity" : 0}, {"y" : 0, "x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0, "triggerHook" : "#master"}'>
+		<span class="pattern" data-scrollmagic='{"tween":[{"y": 50, "x" : -50, "opacity" : 0}, {"y" : 0, "x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0, "triggerElement" : "#master"}'></span>
+		<span class="square" data-scrollmagic='{"tween":[{"y": 50, "opacity" : 0}, {"y" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0, "triggerElement" : "#master"}'></span>
+		<div class="master-content" data-scrollmagic='{"tween":[{"y": 50, "x" : 50, "opacity" : 0}, {"y" : 0, "x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0, "triggerElement" : "#master"}'>
 			<?php the_sub_field('testo_master'); ?>
-			<a href="<?php bloginfo('url'); ?>" class="logo" data-scrollmagic='{"tween":[{"y": -50, "x" : -50, "opacity" : 0}, {"y" : 0, "x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0, "triggerHook" : "#master"}'>
+			<a href="<?php bloginfo('url'); ?>" class="logo" data-scrollmagic='{"tween":[{"y": -50, "x" : -50, "opacity" : 0}, {"y" : 0, "x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0, "triggerElement" : "#master"}'>
 				<span class="j">J</span>
 				<span class="j">J</span>
 			</a>
