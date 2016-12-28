ad77982ac45e6b78f06a82fb9bdb3ce468329755
tedc <egrandinetti@gmail.com>
2016-12-28 17:39:43 +0100

    zoomer e immagine

--- builder/immagine.php
+++ builder/immagine.php
@@ -1,12 +1,13 @@
 <?php $img = get_sub_field('immagine'); ?>
-<figure class="container-img">
+<figure class="container-img" id="img_<?php echo $row; ?>">
     <img src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>" class="row-img full-img"/>
     <?php $random = rand(0, 2);?>
     <?php if ($random == 0) {
     } ?>
     <?php if ($random == 1){?>
-    <?php echo '<div class="square"></div><div class="square-shadow"></div>' ?>
+    <div class="square" data-scrollmagic='{"tween":[{"y": 100}, {"y" : -50}], "triggerHook" : 0.5, "duration" : "200vh", "triggerElement" : "#img_<?php echo $row; ?>"}'></div>
+    <div class="square-shadow" data-scrollmagic='{"tween":[{"y": -50}, {"y" : 0}], "triggerHook" : 0.5, "duration" : "200vh", "triggerElement" : "#img_<?php echo $row; ?>"}'></div>
     <?php } else { ?>
-    <?php echo '<div class="square-shadow"></div>'?>
+    <div class="square-shadow" data-scrollmagic='{"tween":[{"y": 100}, {"y" : -50}], "triggerHook" : 0.5, "duration" : "200vh", "triggerElement" : "#img_<?php echo $row; ?>"}'></div>
     <?php } ?>
 </figure>
--- builder/zoomer.php
+++ builder/zoomer.php
@@ -1,3 +1,3 @@
-<div class="container row-md zoomer">
+<div class="container row-md zoomer" data-scrollmagic='{"tween":[{"y": 50, "x" : -50, "opacity" : 0}, {"y" : 0, "x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'>
 	<?php the_sub_field('zoomer'); ?>
 </div>
