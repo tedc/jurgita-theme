03cbb0144f139b096cefb7432e35f3b3f591197a
tedc <egrandinetti@gmail.com>
2016-12-28 18:01:37 +0100

    colonne e instagram

--- builder/instagram.php
+++ builder/instagram.php
@@ -2,12 +2,12 @@
     <h2 class="title aligncenter"><?php _e('Jurgita su Instagram', 'jurgita') ?></h2>
     <div class="container-img">
         <?php echo do_shortcode('[instagram-feed]') ?>
-        <a href="<?php bloginfo('url'); ?>" class="logo">
+        <a href="<?php bloginfo('url'); ?>" class="logo" data-scrollmagic='{"tween":[{"y": -50, "opacity" : 0}, {"y" : 0, "opacity" : 1}], "triggerHook" : 0.75, "duration" : "100%", "triggerElement": "#row_<?php echo $row; ?>"}'>
             <span class="j">J</span>
             <span class="j">J</span>
         </a>
     </div>
     <div class="container-btn">
-        <a href="<?php echo get_option("wpseo_social")["instagram_url"] ?>" class="btn" target="_blank"><?php _e('Segui', 'jurgita') ?></a>
+        <a href="<?php echo get_option("wpseo_social")["instagram_url"] ?>" class="btn" target="_blank"  data-scrollmagic='{"tween":[{"y": -50, "opacity" : 0}, {"y" : 0, "opacity" : 1}], "triggerHook" : 0.75, "duration" : "100%", "triggerElement": "#row_<?php echo $row; ?>"}'><?php _e('Segui', 'jurgita') ?></a>
     </div>
 </div>
