e311a024fee57a3ac9bba8de6bc13fa1fc8dcd14
Mattia <guzzi.mattia@gmail.com>
2016-12-28 12:09:44 +0100

    no message

--- builder/programma.php
+++ builder/programma.php
@@ -3,7 +3,7 @@
     <div class="grid-4 container programma-corsi">
         <?php while (have_rows('giorni')) : the_row(); ?>
             <h2 class="title title-corso"><?php the_sub_field('titolo'); ?></h2>
-                <ul class="argomenti">
+                <ul class="argomenti grid-4">
                     <?php while (have_rows('argomenti')) : the_row(); ?>
                         <li class="argomento col-2">
                             <span class="check"></span>
