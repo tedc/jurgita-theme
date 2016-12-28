a4f26d45b05947745c9aa61fcf44e2368ddf2160
tedc <egrandinetti@gmail.com>
2016-12-28 16:39:12 +0100

    panel btn

--- templates/panel.php
+++ templates/panel.php
@@ -71,8 +71,7 @@ $corsi = new WP_Query(array(
                 </ul>
             </div>
             <a href="<?php the_field('contact_page'); ?>" class="btn btn-contact"><?php _e('Contattami'); ?></a>
-        </div>
-        <span class="panel-btn"><?php _e('Prossimi corsi', 'jurgita'); ?><i class="icon-arrow-down"></i></span>       
-        <a class="main-permalink" href="#" data-reveal="#panel"></a>
+        </div>    
+        <a class="main-permalink" href="#" data-reveal="#panel"><span class="panel-btn"><?php _e('Prossimi corsi', 'jurgita'); ?><i class="icon-arrow-down"></i></span></a>
     </aside>
 <?php endif; ?>
