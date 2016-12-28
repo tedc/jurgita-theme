30189ba70efa59ac35fc037e7cddfe63ce104406
tedc <egrandinetti@gmail.com>
2016-12-28 16:07:18 +0100

    animations

--- builder/divider.php
+++ builder/divider.php
@@ -1,7 +1,7 @@
-<div class="divider container">
-    <div class="square-gray"></div>
-    <div class="square-yellow"></div>
-    <a href="<?php bloginfo('url'); ?>" class="logo">
+<div class="divider container" id="row_<?php echo $row; ?>">
+    <div class="square-gray" data-scrollmagic='{"tween":[{"y": -50, "opacity" : 0, "x" : 50}, {"y" : 0, "opacity" : 1, "x" : 0}], "triggerHook" : 0.5, "triggerElement": "#row_<?php echo $row; ?>"}'></div>
+    <div class="square-yellow" data-scrollmagic='{"tween":[{"y": 50, "opacity" : 0}, {"y" : 0, "opacity" : 1}], "triggerHook" : 0.5, "triggerElement": "#row_<?php echo $row; ?>"}'></div>
+    <a href="<?php bloginfo('url'); ?>" class="logo" data-scrollmagic='{"tween":[{"y": -50, "opacity" : 0, "x" : -50}, {"y" : 0, "opacity" : 1, "x" : 0}], "triggerHook" : 0.5, "triggerElement": "#row_<?php echo $row; ?>"}'>
         <span class="j">J</span>
         <span class="j">J</span>
     </a>
