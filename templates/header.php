1350a42e3fcf120b359a6b5f99b47d4c0eafac3c
Mattia <guzzi.mattia@gmail.com>
2016-12-28 10:11:45 +0100

    no message

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,6 @@
 		<span class="j">J</span>
 	</a>
 	<div class="menu-container">
-		<?php icl_selector(); ?>
 		<span class="btn-header btn-shop"><a href="<?php echo get_permalink( $shop ); ?>" title="<?php echo get_the_title($shop); ?>" class="btn-header-text"><?php _e('Shop', 'jurgita'); ?></a><?php woocommerce_car_icon(); ?></span>
 		<?php wp_nav_menu( ['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container_id' => 'menu', 'container_class' => 'menu'] ); ?>
 		<a class="btn-header btn-menu" href="#" data-reveal="#menu">
@@ -16,6 +15,7 @@
 				<span class="toggle-line"></span>
 			</span>
 		</a>
+		<?php icl_selector(); ?>
 	</div>
 </header>
 <?php get_template_part( 'templates/panel' ); ?>
