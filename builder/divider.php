b7474f0b37bae13e97d2ddc2689f03575b4980ac
tedc <egrandinetti@gmail.com>
2016-12-28 15:23:02 +0100

    push

--- builder/divider.php
+++ builder/divider.php
@@ -1,7 +1,7 @@
 <div class="divider container">
     <div class="square-gray"></div>
     <div class="square-yellow"></div>
-    <a href="http://jurgita.dnative.com" class="logo">
+    <a href="<?php bloginfo('url'); ?>" class="logo">
         <span class="j">J</span>
         <span class="j">J</span>
     </a>
--- builder/facebook.php
+++ builder/facebook.php
@@ -1,14 +1,14 @@
 <div class="container facebook row-lg" id="row_<?php echo $row; ?>">
-    <span class="pattern" data-scrollmagic='{"tween":[{"y": -100}, {"y" : 100}], "duration" : "200vh", "triggerHook" : 1, "triggerElement": "#row_<?php echo $row; ?>"}'></span>
-    <span class="square" data-scrollmagic='{"tween":[{"y": 100}, {"y" : -100}], "duration" : "200vh", "triggerHook" : 1, "triggerElement": "#row_<?php echo $row; ?>"}'></span>
-    <div class="gray-square" data-scrollmagic='{"tween":[{"y": -100}, {"y" : 100}], "duration" : "200vh", "triggerHook" : 1, "triggerElement": "#row_<?php echo $row; ?>"}'></div>
+    <span class="pattern" data-scrollmagic='{"tween":[{"y": -50}, {"y" : 50}], "duration" : "200vh", "triggerHook" : 1, "triggerElement": "#row_<?php echo $row; ?>"}'></span>
+    <span class="square" data-scrollmagic='{"tween":[{"y": 50}, {"y" : -50}], "duration" : "200vh", "triggerHook" : 1, "triggerElement": "#row_<?php echo $row; ?>"}'></span>
+    <div class="gray-square" data-scrollmagic='{"tween":[{"y": -50}, {"y" : 50}], "duration" : "200vh", "triggerHook" : 1, "triggerElement": "#row_<?php echo $row; ?>"}'></div>
     <div class="like-content">
        
         <p class="title" data-scrollmagic='{"tween":{"text":"<?php echo facebook_ratings(); ?> <?php _e('Like', 'jurgita'); ?>", "opacity" : 1}, "duration" : 0, "triggerHook" : 0.75, "reverse" : false}'><?php for($i=0; $i<strlen(facebook_ratings()); $i++) {echo 0; } ?> <?php _e('Like', 'jurgita'); ?></p>
-        <span class="like" data-scrollmagic='{"tween":[{"y": -50}, {"y" : 50}], "duration" : "200vh", "triggerHook" : 1, "triggerElement": "#row_<?php echo $row; ?>"}'>
+        <span class="like" data-scrollmagic='{"tween":[{"y": 50}, {"y" : -50}], "duration" : "200vh", "triggerHook" : 1, "triggerElement": "#row_<?php echo $row; ?>"}'>
             <img class="facebook-like" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/like.svg' ?>">
         </span>
-        <a href="<?php bloginfo('url'); ?>" class="logo" data-scrollmagic='{"tween":[{"y": "-100%"}, {"y" : "0%"}], "duration" : "200vh", "triggerHook" : 1, "triggerElement": "#row_<?php echo $row; ?>"}'>
+        <a href="<?php bloginfo('url'); ?>" class="logo" data-scrollmagic='{"tween":[{"y": "-50%"}, {"y" : "0%"}], "duration" : "200vh", "triggerHook" : 1, "triggerElement": "#row_<?php echo $row; ?>"}'>
             <span class="j">J</span>
             <span class="j">J</span>
         </a>
