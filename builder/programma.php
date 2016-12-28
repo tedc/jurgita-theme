5321efcc37db0efbc740b133c954e9e9ac5b4b38
Mattia <guzzi.mattia@gmail.com>
2016-12-28 11:50:25 +0100

    no message

--- builder/programma.php
+++ builder/programma.php
@@ -4,7 +4,13 @@
     <?php while ( have_rows('giorni') ) : the_row(); ?>
         <h2 class="title title-corso"><?php the_sub_field('titolo'); ?></h2>
         <div class="col-2">
-
+            <ul class="argomenti">
+        <?php while ( have_rows('argomenti') ) : the_row(); ?>
+                <li class="argomento">
+                            <span class="check"> </span>
+           <span class="argomento-text"> <?php the_sub_field('argomento'); ?></span>
+        <?php endwhile ?>
+                </ul>
         </div>
     <?php endwhile ?>
 </div>
