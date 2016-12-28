6cf242578fdbe986418f7a10f3081d71e0772660
tedc <egrandinetti@gmail.com>
2016-12-28 17:22:39 +0100

    , "triggerHook" : "#master"

--- builder/colonne.php
+++ builder/colonne.php
@@ -36,12 +36,12 @@ $dataScrollMagic = ($col%2==0) ? 'data-scrollmagic=\'{"tween":[{"y": 50, "x" : -
 	</div>
 </div>
 <?php elseif (get_row_layout() == 'master') : ?>
-	<div class="container master">
-		<span class="pattern" data-scrollmagic='{"tween":[{"y": 50, "x" : -50, "opacity" : 0}, {"y" : 0, "x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'></span>
-		<span class="square" data-scrollmagic='{"tween":[{"y": 50, "opacity" : 0}, {"y" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'></span>
-		<div class="master-content" data-scrollmagic='{"tween":[{"y": 50, "x" : 50, "opacity" : 0}, {"y" : 0, "x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'>
+	<div class="container master" id="master">
+		<span class="pattern" data-scrollmagic='{"tween":[{"y": 50, "x" : -50, "opacity" : 0}, {"y" : 0, "x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0, "triggerHook" : "#master"}'></span>
+		<span class="square" data-scrollmagic='{"tween":[{"y": 50, "opacity" : 0}, {"y" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0, "triggerHook" : "#master"}'></span>
+		<div class="master-content" data-scrollmagic='{"tween":[{"y": 50, "x" : 50, "opacity" : 0}, {"y" : 0, "x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0, "triggerHook" : "#master"}'>
 			<?php the_sub_field('testo_master'); ?>
-			<a href="<?php bloginfo('url'); ?>" class="logo" data-scrollmagic='{"tween":[{"y": -50, "x" : -50, "opacity" : 0}, {"y" : 0, "x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'>
+			<a href="<?php bloginfo('url'); ?>" class="logo" data-scrollmagic='{"tween":[{"y": -50, "x" : -50, "opacity" : 0}, {"y" : 0, "x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0, "triggerHook" : "#master"}'>
 				<span class="j">J</span>
 				<span class="j">J</span>
 			</a>
