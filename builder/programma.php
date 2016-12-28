60593153488bfcf69ce9dc732b3e834b14043eed
Mattia <guzzi.mattia@gmail.com>
2016-12-28 12:22:50 +0100

    no message

--- builder/programma.php
+++ builder/programma.php
@@ -1,4 +1,4 @@
-<h2 class="title"><?php the_sub_field('titolo') ?></h2>
+<h2 class="title aligncenter"><?php the_sub_field('titolo') ?></h2>
 <?php if (have_rows('giorni')): ?>
     <div class="grid-4 container programma-corsi">
         <?php while (have_rows('giorni')) : the_row(); ?>
