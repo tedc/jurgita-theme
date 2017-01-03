b05c485c87928a8015dc1da4e8fa82ec49dd2f0f
Mattia <guzzi.mattia@gmail.com>
2017-01-03 12:15:36 +0100

    no message

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -288,16 +288,16 @@
         $('.single-image-slider-nav .zoom').click(function(e){
             e.preventDefault();
             console.log('prova');
-            var photo_fullsize =  $(this).find('img').attr('src').replace('-180x180','-600x600');;
+            var photo_fullsize =  $(this).find('img').attr('src').replace('-180x180','-600x600');
             console.log(photo_fullsize);
             var title =  $(this).find('img').attr('alt');
             console.log(title);
-            $('.woocommerce-main-image img').attr('srcset',photo_fullsize);
-            $('.woocommerce-main-image img').attr('src',photo_fullsize);
-            $('.woocommerce-main-image ').attr('href',photo_fullsize);
+            $('.woocommerce-main-image img').attr('srcset', photo_fullsize);
+            $('.woocommerce-main-image img').attr('src', photo_fullsize);
+            $('.woocommerce-main-image ').attr('href', photo_fullsize);
 
-            $('.woocommerce-main-image ').attr('title',title);
-            $('.woocommerce-main-image img').attr('alt',title);
+            $('.woocommerce-main-image ').attr('title', title);
+            $('.woocommerce-main-image img').attr('alt', title);
         });
     }
 
