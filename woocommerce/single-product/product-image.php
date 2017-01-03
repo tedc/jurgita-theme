2ca677ee07d09cfe909cacb001aba4c068c2e129
Mattia <guzzi.mattia@gmail.com>
2017-01-03 15:03:15 +0100

    no message

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -37,7 +37,7 @@ global $post, $product;
 			echo apply_filters(
 				'woocommerce_single_product_image_html',
 				sprintf(
-					'<a href="%s" itemprop="image" class="woocommerce-main-image zoom" title="%s" data-rel="prettyPhoto%s">%s</a>',
+					'<a href="" itemprop="image" class="woocommerce-main-image zoom" title="%s" data-rel="prettyPhoto%s">%s</a>',
 					esc_url( $props['url'] ),
 					esc_attr( $props['caption'] ),
 					$gallery,
