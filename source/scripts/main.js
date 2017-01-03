230bbba05aecbd1539ceeed728b19d984656a73b
Mattia <guzzi.mattia@gmail.com>
2017-01-03 12:26:33 +0100

    no message

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -23,7 +23,7 @@
                 price();
                 disableButtonCart();
                 select.init();
-
+                productImg();
                 awards();
                 faceSwitcher();
                 swichTab();
@@ -282,6 +282,19 @@
             controller.scrollTo(id);
             $(this).addClass('active');
         })
+    }
+
+    function productImg() {
+        $('.single-image-slider-nav .zoom').click(function(e){
+            e.preventDefault();
+            var photo_fullsize =  $(this).find('img').attr('src').replace('-180x180','-600x600');
+            var title =  $(this).find('img').attr('alt');
+            $('.woocommerce-main-image img').attr('srcset', photo_fullsize);
+            $('.woocommerce-main-image img').attr('src', photo_fullsize);
+            $('.woocommerce-main-image ').attr('href', photo_fullsize);
+            $('.woocommerce-main-image ').attr('title', title);
+            $('.woocommerce-main-image img').attr('alt', title);
+        });
     }
 
     function faq() {
