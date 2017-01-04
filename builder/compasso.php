0dfbf5f7f70dc070304a9a10e49fd6289d2cad46
Mattia <guzzi.mattia@gmail.com>
2017-01-04 18:36:47 +0100

    no message

--- builder/compasso.php
+++ builder/compasso.php
@@ -5,5 +5,8 @@
         <div class="slider" id="first-slider"><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
         <div class="dot-icon" id="first-dot">1</div>
 
+        <div class="slider" id="second-slider"><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
+        <div class="dot-icon" id="second-dot">2</div>
+
     </div>
 </div>
