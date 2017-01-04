41419812d4bf8440ed731d5ca1e74efb7faa9d44
Mattia <guzzi.mattia@gmail.com>
2017-01-04 11:29:40 +0100

    no message

--- builder/livelli.php
+++ builder/livelli.php
@@ -6,22 +6,24 @@ if ($terms): ?>
     <div class="grid-4 container">
         <?php foreach ($terms as $term): ?>
             <div class="col-2">
-                <h2 class="title title-corso" data-scrollmagic='{"tween":[{"x": -50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'><?php echo $term->name; ?></h2>
+                <h2 class="title title-corso"
+                    data-scrollmagic='{"tween":[{"x": -50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'><?php echo $term->name; ?></h2>
                 <ul class="argomenti">
                     <?php while (have_rows('argomenti', 'livelli_' . $term->term_id)) : the_row(); ?>
-                        <li class="argomento" data-scrollmagic='{"tween":[{"x": -50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'>
-                            <span class="check" data-scrollmagic='{"tween":[{"x": 20, "y" : 20,  "opacity" : 0}, {"x" : 0, "y" : 0, "opacity" : 1, "delay" : 0.25}], "triggerHook" : 0.5, "duration" : 0}'>
+                        <li class="argomento"
+                            data-scrollmagic='{"tween":[{"x": -50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'>
+                            <span class="check"
+                                  data-scrollmagic='{"tween":[{"x": 20, "y" : 20,  "opacity" : 0}, {"x" : 0, "y" : 0, "opacity" : 1, "delay" : 0.25}], "triggerHook" : 0.5, "duration" : 0}'>
                                 <?php echo (get_sub_field('check')) ? '<i class="icon-check"></i>' : ''; ?>
                             </span>
-                            <?php echo (get_sub_field('check')) ? '<span class="argomento-text">'.get_sub_field('argomento').'</span>' : ''; ?>
+                            <?php echo (get_sub_field('check')) ? '<span class="argomento-text">' . get_sub_field('argomento') . '</span>' : ''; ?>
                         </li>
-                  <?php  endwhile; ?>
-                  </ul>
-                <?php while (have_rows('prezzo', 'livelli_' . $term->term_id)) : the_row(); ?>
-                    <div class="prova">
-                    <?php echo (get_sub_field('prezzo')) ? '<div class="price">'.get_sub_field('prezzo').'</div>' : ''; ?>
-                    </div>
-                <?php  endwhile; ?>
+                    <?php endwhile; ?>
+                </ul>
+
+
+                <div class="price"> <?php the_field('prezzo', $term) ?></div>
+     
             </div>
         <?php endforeach; ?>
     </div>
