1b2ea20bf0df2094ed6e3f3dfa4204f228100831
Mattia <guzzi.mattia@gmail.com>
2017-01-04 11:58:27 +0100

    no message

--- builder/livelli.php
+++ builder/livelli.php
@@ -21,7 +21,7 @@ if ($terms): ?>
                 <?php endwhile; ?>
             </ul>
 
-            <?php if(the_field('prezzo', $term)) { ?>
+            <?php if( get_field('prezzo', $term) ) { ?>
             <div class="price corso-price" data-scrollmagic='{"tween":[{"x": 50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'> <?php the_field('prezzo', $term) ?></div>
             <?php } ?>
         </div>
