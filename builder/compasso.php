ea134ff48ef060dc6601ea8ad5c353fd0a864f59
Mattia <guzzi.mattia@gmail.com>
2017-01-04 16:44:48 +0100

    no message

--- builder/compasso.php
+++ builder/compasso.php
@@ -1,3 +1,18 @@
-<div class="compasso">
-    <img src="<?php the_sub_field('immagine'); ?>"
+<div class="body-container compasso">
+    <img src="<?php the_sub_field('immagine'); ?>">
+    <div class="mask first">
+        <div class="dot-icon">+</div>
+    </div>
+    <div class="mask second">
+        <div class="dot-icon">+</div>
+    </div>
+    <div class="mask third">
+        <div class="dot-icon">+</div>
+    </div>
+    <div class="mask fourth">
+        <div class="dot-icon">+</div>
+    </div>
+    <div class="mask fifth">
+        <div class="dot-icon">+</div>
+    </div>
 </div>
