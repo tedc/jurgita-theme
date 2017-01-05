ad6fb949a6da1bcd71331bcb27d731ab8e5ea6bf
Mattia <guzzi.mattia@gmail.com>
2017-01-05 10:38:31 +0100

    no message

--- builder/compasso.php
+++ builder/compasso.php
@@ -1,6 +1,6 @@
 <div class="body-container">
-    <div class="compasso">
-        <div class="container-img" data-scrollmagic='{"tween":[{"x": 50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'>
+    <div class="compasso" data-scrollmagic='{"tween":[{"x": 50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'>
+        <div class="container-img">
             <img src="<?php the_sub_field('immagine'); ?>">
             <div class="dot-icon" id="first-dot">1</div>
             <div class="dot-icon" id="second-dot">2</div>
@@ -9,10 +9,10 @@
             <div class="dot-icon" id="fifth-dot">5</div>
         </div>
 
-        <div class="slider" id="first-slider" data-scrollmagic='{"tween":[{"x": 50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'><span class="circle">1</span><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
-        <div class="slider" id="second-slider" data-scrollmagic='{"tween":[{"x": -50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'><span class="circle">2</span><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
-        <div class="slider" id="third-slider" data-scrollmagic='{"tween":[{"x": 50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'><span class="circle">3</span><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
-        <div class="slider" id="fourth-slider" data-scrollmagic='{"tween":[{"x": -50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'><span class="circle">4</span><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
-        <div class="slider" id="fifth-slider" data-scrollmagic='{"tween":[{"x": 50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'><span class="circle">5</span><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
+        <div class="slider" id="first-slider"><span class="circle">1</span><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
+        <div class="slider" id="second-slider"><span class="circle">2</span><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
+        <div class="slider" id="third-slider"><span class="circle">3</span><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
+        <div class="slider" id="fourth-slider"><span class="circle">4</span><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
+        <div class="slider" id="fifth-slider"><span class="circle">5</span><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
     </div>
 </div>
