ba3c93a6374ea49f2cafafb33f465b5a0a05e264
Mattia <guzzi.mattia@gmail.com>
2017-01-05 09:49:47 +0100

    no message

--- builder/compasso.php
+++ builder/compasso.php
@@ -1,20 +1,18 @@
 <div class="body-container">
     <div class="compasso">
-        <img src="<?php the_sub_field('immagine'); ?>">
+        <div class="container-img">
+            <img src="<?php the_sub_field('immagine'); ?>">
+            <div class="dot-icon" id="first-dot">1</div>
+            <div class="dot-icon" id="second-dot">2</div>
+            <div class="dot-icon" id="third-dot">3</div>
+            <div class="dot-icon" id="fourth-dot">4</div>
+            <div class="dot-icon" id="fifth-dot">5</div>
+        </div>
 
         <div class="slider" id="first-slider"><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
-        <div class="dot-icon" id="first-dot">1</div>
-
         <div class="slider" id="second-slider"><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
-        <div class="dot-icon" id="second-dot">2</div>
-
         <div class="slider" id="third-slider"><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
-        <div class="dot-icon" id="third-dot">3</div>
-
         <div class="slider" id="fourth-slider"><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
-        <div class="dot-icon" id="fourth-dot">4</div>
-
         <div class="slider" id="fifth-slider"><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
-        <div class="dot-icon" id="fifth-dot">5</div>
     </div>
 </div>
