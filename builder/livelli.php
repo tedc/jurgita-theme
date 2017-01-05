aab4a1f7ce968584000eef770d5d5fe7a2f3cd44
Mattia <guzzi.mattia@gmail.com>
2017-01-05 11:37:50 +0100

    no message

--- builder/livelli.php
+++ builder/livelli.php
@@ -3,17 +3,14 @@
 $terms = get_sub_field('tipologie');
 if ($terms): ?>
 
-    <div class="grid-4 container">
+    <div class="grid-4 container"  data-scrollmagic='{"tween":[{"x": -50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'>
     <?php foreach ($terms as $term): ?>
         <div class="col-2 col-programma">
-            <h2 class="title title-corso"
-                data-scrollmagic='{"tween":[{"x": -50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'><?php echo $term->name; ?></h2>
+            <h2 class="title title-corso"><?php echo $term->name; ?></h2>
             <ul class="argomenti">
                 <?php while (have_rows('argomenti', 'livelli_' . $term->term_id)) : the_row(); ?>
-                    <li class="argomento"
-                        data-scrollmagic='{"tween":[{"x": -50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'>
-                            <span class="check"
-                                  data-scrollmagic='{"tween":[{"x": 20, "y" : 20,  "opacity" : 0}, {"x" : 0, "y" : 0, "opacity" : 1, "delay" : 0.25}], "triggerHook" : 0.5, "duration" : 0}'>
+                    <li class="argomento">
+                            <span class="check">
                                 <?php echo (get_sub_field('check')) ? '<i class="icon-check"></i>' : ''; ?>
                             </span>
                         <?php echo (get_sub_field('check')) ? '<span class="argomento-text">' . get_sub_field('argomento') . '</span>' : ''; ?>
@@ -24,7 +21,7 @@ if ($terms): ?>
             $int = strtok($price, '.');
             $dec = substr($price, strpos($price, ".")+1);
             ?>
-            <div class="price corso-price" data-scrollmagic='{"tween":[{"x": 50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'><div class="price-container"> <?php echo '€&nbsp;'.'<span class="int-price">'. $int . ',</span>' . $dec ?></div></div>
+            <div class="price corso-price"><div class="price-container"> <?php echo '€&nbsp;'.'<span class="int-price">'. $int . ',</span>' . $dec ?></div></div>
         </div>
     <?php endforeach; ?>
     </div>
