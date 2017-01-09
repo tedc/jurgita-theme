ed2c4749672e46965df6b617f2ce447a6d76cff0
Mattia <guzzi.mattia@gmail.com>
2017-01-09 17:52:10 +0100

    no message

--- templates/sticky.php
+++ templates/sticky.php
@@ -1,4 +1,4 @@
-<div class="container-img col-2">
+<div class="container-img col-2" data-scrollmagic="{"tween":[{"x" : -50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}">
     <figure>
         <img src="<?php the_post_thumbnail_url() ?>">
         <div class="square"></div>
@@ -6,7 +6,7 @@
     </figure>
 </div>
 
-<article <?php post_class("col-2"); ?>
+<article <?php post_class("col-2"); ?> data-scrollmagic="{"tween":[{"x" : +50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}">
     <div class="content-post">
         <header>
             <?php get_template_part('templates/entry-meta'); ?>
