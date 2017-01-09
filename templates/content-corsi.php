339bc84b82568b318fbe394841651d8bfb750c02
Mattia <guzzi.mattia@gmail.com>
2017-01-09 18:55:55 +0100

    no message

--- templates/content-corsi.php
+++ templates/content-corsi.php
@@ -4,10 +4,10 @@
 	<div class="container-img col-2">
 	    <figure>
 		    <a href="<?php the_permalink(); ?>">
-		        <img src="<?php the_post_thumbnail_url( 'large' ) ?>" data-scrollmagic='{"tween":[{"y": 300, "x" : 0}, {"y" : 0, "x" : 0}], "triggerHook" : 0.5, "duration" : "100vh", "triggerElement": "#corsi"}'>
+		        <img src="<?php the_post_thumbnail_url( 'large' ) ?>" data-scrollmagic='{"tween":[{"y": 300, "x" : 0}, {"y" : 0, "x" : 0}], "triggerHook" : 0.5, "duration" : "100vh", "triggerElement": "#corsi-<?php echo $i ?>"}'>
 		    </a>
-	        <div class="square" data-scrollmagic='{"tween":[{"y": -300}, {"y" : 0}], "triggerHook" : 0.5, "duration" : "100vh", "triggerElement": "#corsi"}'></div>
-	        <div class="square-shadow" data-scrollmagic='{"tween":[{"y": 300, "x" : 0}, {"y" : 0, "x" : 0}], "triggerHook" : 0.5, "duration" : "100vh", "triggerElement": "#corsi"}'></div>
+	        <div class="square" data-scrollmagic='{"tween":[{"y": -300}, {"y" : 0}], "triggerHook" : 0.5, "duration" : "100vh", "triggerElement": "#corsi-<?php echo $i ?>"}'></div>
+	        <div class="square-shadow" data-scrollmagic='{"tween":[{"y": 300, "x" : 0}, {"y" : 0, "x" : 0}], "triggerHook" : 0.5, "duration" : "100vh", "triggerElement": "#corsi-<?php echo $i ?>"}'></div>
 	    </figure>
 	</div>
 	<article class="col-2 content" <?php echo $dataScrollMagic ?>>
