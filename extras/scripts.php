930843606d33ae938e3008e7bf6f3d4482082d3a
Mattia <guzzi.mattia@gmail.com>
2017-01-10 14:57:09 +0100

    no message

--- extras/scripts.php
+++ extras/scripts.php
@@ -3,8 +3,8 @@ function vars()
 {
     ?>
     <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
-    <script>
 
+    <script>
         var WebFontConfig = {
             typekit: {
                 id: 'qse6jbb'
@@ -21,6 +21,7 @@ function vars()
         // })(document);
         var isMobile = <?php echo (is_handheld()) ? 'true' : 'false'; ?>;
     </script>
+
     <script type="text/javascript">
         var _iub = _iub || [];
         _iub.csConfiguration = {
@@ -29,8 +30,21 @@ function vars()
             lang: "it"
         };
     </script>
+
     <script type="text/javascript" src="//cdn.iubenda.com/cookie_solution/safemode/iubenda_cs.js" charset="UTF-8"
             async></script>
+
+    <!-- Hotjar Tracking Code for www.jurgitajasiunaite.com -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:385165,hjsv:5};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
     <?php
 }
 
