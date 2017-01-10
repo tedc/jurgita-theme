dca3510bc750e9a5ae2b5b7ec0f3165b0f692eab
Mattia <guzzi.mattia@gmail.com>
2017-01-10 12:37:15 +0100

    no message

--- extras/scripts.php
+++ extras/scripts.php
@@ -1,33 +1,45 @@
 <?php
-	function vars() {
-		?>
-	<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
-	<script>
+function vars()
+{
+    ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+    <script>
 
-		var WebFontConfig = {
-			typekit : {
-				id : 'qse6jbb'
-			},
-			google : {
-				families : ['Noto Sans:400,400italic,700,700italic', 'Noto Serif:400']
-			}
-		};
-		WebFont.load(WebFontConfig);
-		// (function(d) {
-		// 	var wf = d.createElement('script'), s = d.scripts[0];
-		// 	wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
-		// 	s.parentNode.insertBefore(wf, s);
-		// })(document);
-		var isMobile = <?php echo (is_handheld()) ? 'true' : 'false'; ?>;
-	</script>
-<?php
-	}
-	add_action('wp_footer', 'vars');?>
+        var WebFontConfig = {
+            typekit: {
+                id: 'qse6jbb'
+            },
+            google: {
+                families: ['Noto Sans:400,400italic,700,700italic', 'Noto Serif:400']
+            }
+        };
+        WebFont.load(WebFontConfig);
+        // (function(d) {
+        // 	var wf = d.createElement('script'), s = d.scripts[0];
+        // 	wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
+        // 	s.parentNode.insertBefore(wf, s);
+        // })(document);
+        var isMobile = <?php echo (is_handheld()) ? 'true' : 'false'; ?>;
+    </script>
+    <script type="text/javascript">
+        var _iub = _iub || [];
+        _iub.csConfiguration = {
+            cookiePolicyId: 7991549,
+            siteId: 686446,
+            lang: "it"
+        };
+    </script>
+    <script type="text/javascript" src="//cdn.iubenda.com/cookie_solution/safemode/iubenda_cs.js" charset="UTF-8"
+            async></script>
+    <?php
+}
 
+add_action('wp_footer', 'vars'); ?>
 
 
 <?php
 add_action('wp_footer', 'add_googleanalytics');
-function add_googleanalytics() { ?>
-	<!-- Incollare qui codice google analytics -->
+function add_googleanalytics()
+{ ?>
+    <!-- Incollare qui codice google analytics -->
 <?php } ?>
