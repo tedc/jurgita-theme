79fc03a86a4f48c37a1fbd74af83c77fc640e567
Mattia <guzzi.mattia@gmail.com>
2017-01-11 16:18:55 +0100

    no message

--- extras/setup.php
+++ extras/setup.php
@@ -1,18 +1,5 @@
 <?php
 
-//Removes Title and Description on CPT Archive
-remove_action( 'genesis_before_loop', 'genesis_do_cpt_archive_title_description' );
-//Removes Title and Description on Blog Archive
-remove_action( 'genesis_before_loop', 'genesis_do_posts_page_heading' );
-//Removes Title and Description on Date Archive
-remove_action( 'genesis_before_loop', 'genesis_do_date_archive_title' );
-//Removes Title and Description on Archive, Taxonomy, Category, Tag
-remove_action( 'genesis_before_loop', 'genesis_do_taxonomy_title_description', 15 );
-//Removes Title and Description on Author Archive
-remove_action( 'genesis_before_loop', 'genesis_do_author_title_description', 15 );
-//Removes Title and Description on Blog Template Page
-remove_action( 'genesis_before_loop', 'genesis_do_blog_template_heading' );
-
 function main_menu_footer($nav_menu, $args)
 {
     if ($args->theme_location === 'primary_navigation') :
