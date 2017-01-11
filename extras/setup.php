f382603edfd6d03f56cab519a591c95de63b5cd5
Mattia <guzzi.mattia@gmail.com>
2017-01-11 16:12:09 +0100

    no message

--- extras/setup.php
+++ extras/setup.php
@@ -1,13 +1,24 @@
 <?php
 
-add_filter('get_the_archive_title', 'modify_archive_title');
-function modify_archive_title($title)
-{
-    if (is_category()) {
-        $title = single_cat_title('', false);
+add_filter( 'get_the_archive_title', function ($title) {
+
+    if ( is_category() ) {
+
+        $title = single_cat_title( '', false );
+
+    } elseif ( is_tag() ) {
+
+        $title = single_tag_title( '', false );
+
+    } elseif ( is_author() ) {
+
+        $title = '<span class="vcard">' . get_the_author() . '</span>' ;
+
     }
+
     return $title;
-}
+
+});
 
 function main_menu_footer($nav_menu, $args)
 {
