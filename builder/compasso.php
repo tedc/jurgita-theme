09d490191b670f26343417b0244ff1e71d91f2bc
Mattia <guzzi.mattia@gmail.com>
2017-01-18 15:25:59 +0100

    no message

--- builder/compasso.php
+++ builder/compasso.php
@@ -1,5 +1,5 @@
-<div class="body-container">
-    <div class="compasso" data-scrollmagic='{"tween":[{"x": -50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'>
+<div class="body-container" data-scrollmagic='{"tween":[{"opacity" : 0}, {"opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'>
+    <div class="compasso">
         <div class="container-img">
             <img src="<?php the_sub_field('immagine'); ?>">
            <!-- <div class="dot-icon" id="first-dot">1</div>
@@ -16,12 +16,12 @@
     </div>
 </div>
 <?php if (have_rows('componenti')): ?>
-    <div class="grid-4 container programma-corsi">
+    <div class="container programma-corsi row-lg">
             <ul class="argomenti grid-4">
                 <?php $i = 1; ?>
                 <?php while (have_rows('componenti')) : the_row(); ?>
-                    <li class="argomento col-2" data-scrollmagic='{"tween":[{"x": -50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'>
-                        <span class="check" data-scrollmagic='{"tween":[{"x": 20, "y" : 20,  "opacity" : 0}, {"x" : 0, "y" : 0, "opacity" : 1, "delay" : 0.25}], "triggerHook" : 0.5, "duration" : 0}'><i class="number"><?php echo $i; $i++;?></i></span>
+                    <li class="argomento col-2">
+                        <span class="check"><i class="number"><?php echo $i; $i++;?></i></span>
                         <span class="argomento-text"> <?php the_sub_field('testo'); ?></span>
                     </li>
                 <?php endwhile ?>
