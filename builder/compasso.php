679497cd47f935c4ae36a456cbd5f220ad41e9f0
Mattia <guzzi.mattia@gmail.com>
2017-01-18 13:00:40 +0100

    no message

--- builder/compasso.php
+++ builder/compasso.php
@@ -2,17 +2,29 @@
     <div class="compasso" data-scrollmagic='{"tween":[{"x": -50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'>
         <div class="container-img">
             <img src="<?php the_sub_field('immagine'); ?>">
-            <div class="dot-icon" id="first-dot">1</div>
+           <!-- <div class="dot-icon" id="first-dot">1</div>
             <div class="dot-icon" id="second-dot">2</div>
             <div class="dot-icon" id="third-dot">3</div>
             <div class="dot-icon" id="fourth-dot">4</div>
-            <div class="dot-icon" id="fifth-dot">5</div>
+            <div class="dot-icon" id="fifth-dot">5</div>-->
         </div>
-
-        <div class="slider" id="first-slider"><span class="circle">1</span><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
-        <div class="slider" id="second-slider"><span class="circle">2</span><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
-        <div class="slider" id="third-slider"><span class="circle">3</span><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
-        <div class="slider" id="fourth-slider"><span class="circle">4</span><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
-        <div class="slider" id="fifth-slider"><span class="circle">5</span><?php _e('1 compasso PhiBros', 'jurgita'); ?></div>
+       <!-- <div class="slider" id="first-slider"><span class="circle">1</span><?php /*_e('1 compasso PhiBros', 'jurgita'); */?></div>
+        <div class="slider" id="second-slider"><span class="circle">2</span><?php /*_e('1 compasso PhiBros', 'jurgita'); */?></div>
+        <div class="slider" id="third-slider"><span class="circle">3</span><?php /*_e('1 compasso PhiBros', 'jurgita'); */?></div>
+        <div class="slider" id="fourth-slider"><span class="circle">4</span><?php /*_e('1 compasso PhiBros', 'jurgita'); */?></div>
+        <div class="slider" id="fifth-slider"><span class="circle">5</span><?php /*_e('1 compasso PhiBros', 'jurgita'); */?></div>-->
     </div>
 </div>
+<?php if (have_rows('componenti')): ?>
+    <div class="grid-4 container programma-corsi">
+            <ul class="argomenti grid-4">
+                <?php $i = 1; ?>
+                <?php while (have_rows('componenti')) : the_row(); ?>
+                    <li class="argomento col-2" data-scrollmagic='{"tween":[{"x": -50, "opacity" : 0}, {"x" : 0, "opacity" : 1}], "triggerHook" : 0.5, "duration" : 0}'>
+                        <span class="check" data-scrollmagic='{"tween":[{"x": 20, "y" : 20,  "opacity" : 0}, {"x" : 0, "y" : 0, "opacity" : 1, "delay" : 0.25}], "triggerHook" : 0.5, "duration" : 0}'><i class="number"><?php echo $i; $i++;?></i></span>
+                        <span class="argomento-text"> <?php the_sub_field('testo'); ?></span>
+                    </li>
+                <?php endwhile ?>
+            </ul>
+    </div>
+<?php endif ?>
