ec05e60ab57894f73ee925a95fd65bf6b676225c
Mattia <guzzi.mattia@gmail.com>
2017-01-25 16:49:59 +0100

    no message

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,8 @@
 		<span class="j">J</span>
 	</a>
 	<div class="menu-container">
-		<span class="btn-header btn-shop"><a href="<?php echo get_permalink( $shop ); ?>" title="<?php echo get_the_title($shop); ?>" class="btn-header-text"><?php _e('Shop', 'jurgita'); ?></a><?php woocommerce_car_icon(); ?></span>
+		<span class="btn-header btn-shop"><a href="<?php echo get_permalink( $shop ); ?>" title="<?php echo get_the_title($shop); ?>" class="btn-header-text"><?php _e('Shop', 'jurgita'); ?></a></span>
+		<span class="btn-header btn-shop"><?php woocommerce_car_icon(); ?></span>
 		<?php wp_nav_menu( ['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container_id' => 'menu', 'container_class' => 'menu'] ); ?>
 		<a class="btn-header btn-menu" href="#" data-reveal="#menu">
 			<span class="btn-header-text"><?php _e('Menu', 'jurgita'); ?></span>
