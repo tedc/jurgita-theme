91a67528a9d159449697036ee54c19e985e93048
Mattia <guzzi.mattia@gmail.com>
2017-02-02 16:20:14 +0100

    no message

--- templates/header.php
+++ templates/header.php
@@ -5,8 +5,7 @@
 		<span class="j">J</span>
 	</a>
 	<div class="menu-container">
-		<span class="btn-header btn-shop"><a href="<?php echo get_permalink( $shop ); ?>" title="<?php echo get_the_title($shop); ?>" class="btn-header-text"><?php _e('Shop', 'jurgita'); ?></a></span>
-		<span class="btn-header btn-cart"><?php woocommerce_car_icon(); ?></span>
+		<!--<span class="btn-header btn-shop"><a href="<?php /*echo get_permalink( $shop ); */?>" title="<?php /*echo get_the_title($shop); */?>" class="btn-header-text"><?php /*_e('Shop', 'jurgita'); */?></a></span>-->
 		<?php wp_nav_menu( ['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container_id' => 'menu', 'container_class' => 'menu'] ); ?>
 		<a class="btn-header btn-menu" href="#" data-reveal="#menu">
 			<span class="btn-header-text"><?php _e('Menu', 'jurgita'); ?></span>
@@ -16,6 +15,7 @@
 				<span class="toggle-line"></span>
 			</span>
 		</a>
+		<span class="btn-header btn-cart"><?php woocommerce_car_icon(); ?></span>
 		<?php icl_selector(); ?>
 	</div>
 </header>
