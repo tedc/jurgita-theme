08bb85722900362a928a9982980093f43c1557c5
Mattia <guzzi.mattia@gmail.com>
2017-02-24 11:05:31 +0100

    no message

--- lib/extras.php
+++ lib/extras.php
@@ -32,6 +32,8 @@ function excerpt_more()
   return ' &hellip;';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+
 /*
 function _add_https( $src ){
    return str_replace('http:', 'https:', $src);
